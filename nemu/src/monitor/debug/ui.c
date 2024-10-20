494541f20cbdd23d883f4264443afbb77cb29ff5
tracer-ics2020 <tracer@njuics.org>
2024-10-20 18:57:12 +0800

    >  compile 191220000  Static hostname: DESKTOP-75S465I        Icon name: computer-container          Chassis: container       Machine ID: 1c9f91d336034cdebf733748cf9de30a          Boot ID: 8a698928798a42b89db2e096bcc37629   Virtualization: wsl Operating System: Ubuntu 22.04.5 LTS           Kernel: Linux 5.15.153.1-microsoft-standard-WSL2     Architecture: x86-64  18:57:12 up  6:39,  1 user,  load average: 0.19, 0.09, 0.02

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -10,64 +10,107 @@ void cpu_exec(uint64_t);
 int is_batch_mode();
 
 /* We use the `readline' library to provide more flexibility to read from stdin. */
-static char* rl_gets() {
+static char *rl_gets()
+{
   static char *line_read = NULL;
 
-  if (line_read) {
+  if (line_read)
+  {
     free(line_read);
     line_read = NULL;
   }
 
   line_read = readline("(nemu) ");
 
-  if (line_read && *line_read) {
+  if (line_read && *line_read)
+  {
     add_history(line_read);
   }
 
   return line_read;
 }
 
-static int cmd_c(char *args) {
+static int cmd_c(char *args)
+{
   cpu_exec(-1);
   return 0;
 }
 
-
-static int cmd_q(char *args) {
+static int cmd_q(char *args)
+{
   return -1;
 }
 
 static int cmd_help(char *args);
-
-static struct {
+static int cmd_si(char *args);
+static int cmd_info(char *args);
+static int cmd_x(char *args);
+static int cmd_p(char *args);
+static int cmd_w(char *args);
+static int cmd_dw(char *args);
+static struct
+{
   char *name;
   char *description;
-  int (*handler) (char *);
-} cmd_table [] = {
-  { "help", "Display informations about all supported commands", cmd_help },
-  { "c", "Continue the execution of the program", cmd_c },
-  { "q", "Exit NEMU", cmd_q },
+  int (*handler)(char *);
+} cmd_table[] = {
+    {"help", "Display informations about all supported commands", cmd_help},
+    {"c", "Continue the execution of the program", cmd_c},
+    {"q", "Exit NEMU", cmd_q},
+    {"si", "step any instuctions and stop", cmd_si},
+    {"s", "step any instuctions and stop", cmd_si},
+    {"info", "print the info of r and w", cmd_info},
+    {"i", "print the info of r and w", cmd_info},
+    {"x", "print the val of given arg", cmd_x},
+    {"p", "run expr", cmd_p},
+    {"watch", "add watch point", cmd_w},
+    {"w", "add watch point", cmd_w},
+    {"delete", "delete watch point", cmd_dw},
+    {"d", "delete watch point", cmd_dw},
 
-  /* TODO: Add more commands */
+    /* TODO: Add more commands */
 
 };
+static int cmd_si(char *args)
+{
+  int n_step = 1;
+  char *arg = strtok(NULL, " ");
+  if (arg == NULL)
+    ;
+  else
+    sscanf(arg, "%d", &n_step);
+  printf("nstep %d \n", n_step);
+  cpu_exec(n_step);
+  return 0;
+}
+static int cmd_info(char *args) { return -1; };
+static int cmd_x(char *args) { return -1; };
+static int cmd_p(char *args) { return -1; };
+static int cmd_w(char *args) { return -1; };
+static int cmd_dw(char *args) { return -1; };
 
 #define NR_CMD (sizeof(cmd_table) / sizeof(cmd_table[0]))
 
-static int cmd_help(char *args) {
+static int cmd_help(char *args)
+{
   /* extract the first argument */
   char *arg = strtok(NULL, " ");
   int i;
 
-  if (arg == NULL) {
+  if (arg == NULL)
+  {
     /* no argument given */
-    for (i = 0; i < NR_CMD; i ++) {
+    for (i = 0; i < NR_CMD; i++)
+    {
       printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
     }
   }
-  else {
-    for (i = 0; i < NR_CMD; i ++) {
-      if (strcmp(arg, cmd_table[i].name) == 0) {
+  else
+  {
+    for (i = 0; i < NR_CMD; i++)
+    {
+      if (strcmp(arg, cmd_table[i].name) == 0)
+      {
         printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
         return 0;
       }
@@ -77,24 +120,31 @@ static int cmd_help(char *args) {
   return 0;
 }
 
-void ui_mainloop() {
-  if (is_batch_mode()) {
+void ui_mainloop()
+{
+  if (is_batch_mode())
+  {
     cmd_c(NULL);
     return;
   }
 
-  for (char *str; (str = rl_gets()) != NULL; ) {
+  for (char *str; (str = rl_gets()) != NULL;)
+  {
     char *str_end = str + strlen(str);
 
     /* extract the first token as the command */
     char *cmd = strtok(str, " ");
-    if (cmd == NULL) { continue; }
+    if (cmd == NULL)
+    {
+      continue;
+    }
 
     /* treat the remaining string as the arguments,
      * which may need further parsing
      */
     char *args = cmd + strlen(cmd) + 1;
-    if (args >= str_end) {
+    if (args >= str_end)
+    {
       args = NULL;
     }
 
@@ -104,13 +154,21 @@ void ui_mainloop() {
 #endif
 
     int i;
-    for (i = 0; i < NR_CMD; i ++) {
-      if (strcmp(cmd, cmd_table[i].name) == 0) {
-        if (cmd_table[i].handler(args) < 0) { return; }
+    for (i = 0; i < NR_CMD; i++)
+    {
+      if (strcmp(cmd, cmd_table[i].name) == 0)
+      {
+        if (cmd_table[i].handler(args) < 0)
+        {
+          return;
+        }
         break;
       }
     }
 
-    if (i == NR_CMD) { printf("Unknown command '%s'\n", cmd); }
+    if (i == NR_CMD)
+    {
+      printf("Unknown command '%s'\n", cmd);
+    }
   }
 }
