15e19bf0faea92f14000783fcbc1a80dface77ce
tracer-ics2020 <tracer@njuics.org>
2024-10-20 20:23:48 +0800

    >  compile 191220000  Static hostname: DESKTOP-75S465I        Icon name: computer-container          Chassis: container       Machine ID: 1c9f91d336034cdebf733748cf9de30a          Boot ID: 8a698928798a42b89db2e096bcc37629   Virtualization: wsl Operating System: Ubuntu 22.04.5 LTS           Kernel: Linux 5.15.153.1-microsoft-standard-WSL2     Architecture: x86-64  20:23:48 up  8:05,  1 user,  load average: 0.09, 0.05, 0.01

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -105,6 +105,7 @@ void cpu_exec(uint64_t n)
     if (check_wp())
     {
       nemu_state.state = NEMU_STOP;
+      asm_print(this_pc, seq_pc - this_pc, n < MAX_INSTR_TO_PRINT);
     }
     /* TODO: check watchpoints here. */
 #endif
