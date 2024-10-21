12302221cbd3ae1a607fc5d2c28eca86b02dd7ca
mzh <1113584652@qq.com>
2024-10-21 17:15:20 +0800

    start

--- nemu/include/cpu/exec.h
+++ nemu/include/cpu/exec.h
@@ -6,26 +6,33 @@
 #include <memory/vaddr.h>
 #include <cpu/decode.h>
 
-#define def_EHelper(name) void concat(exec_, name) (DecodeExecState *s)
+#define def_EHelper(name) void concat(exec_, name)(DecodeExecState * s)
 
 // empty decode helper
 #define decode_empty(s)
-
+// IDEXW(0xb0, mov_I2r, mov, 1)
 #define IDEXW(idx, id, ex, w) CASE_ENTRY(idx, concat(decode_, id), concat(exec_, ex), w)
-#define IDEX(idx, id, ex)     IDEXW(idx, id, ex, 0)
-#define EXW(idx, ex, w)       IDEXW(idx, empty, ex, w)
-#define EX(idx, ex)           EXW(idx, ex, 0)
-#define EMPTY(idx)            EX(idx, inv)
+#define IDEX(idx, id, ex) IDEXW(idx, id, ex, 0)
+#define EXW(idx, ex, w) IDEXW(idx, empty, ex, w)
+#define EX(idx, ex) EXW(idx, ex, 0)
+#define EMPTY(idx) EX(idx, inv)
 
 // set_width() is defined in src/isa/$isa/exec/exec.c
-#define CASE_ENTRY(idx, id, ex, w) case idx: set_width(s, w); id(s); ex(s); break;
+#define CASE_ENTRY(idx, id, ex, w) \
+  case idx:                        \
+    set_width(s, w);               \
+    id(s);                         \
+    ex(s);                         \
+    break;
 
-static inline uint32_t instr_fetch(vaddr_t *pc, int len) {
+static inline uint32_t instr_fetch(vaddr_t *pc, int len)
+{
   uint32_t instr = vaddr_ifetch(*pc, len);
 #ifdef DEBUG
   uint8_t *p_instr = (void *)&instr;
   int i;
-  for (i = 0; i < len; i ++) {
+  for (i = 0; i < len; i++)
+  {
     extern char log_bytebuf[];
     strcatf(log_bytebuf, "%02x ", p_instr[i]);
   }
@@ -34,14 +41,16 @@ static inline uint32_t instr_fetch(vaddr_t *pc, int len) {
   return instr;
 }
 
-static inline void update_pc(DecodeExecState *s) {
+static inline void update_pc(DecodeExecState *s)
+{
   cpu.pc = (s->is_jmp ? s->jmp_pc : s->seq_pc);
 }
 
 #ifdef DEBUG
-#define print_asm(...) \
-  do { \
-    extern char log_asmbuf[]; \
+#define print_asm(...)                \
+  do                                  \
+  {                                   \
+    extern char log_asmbuf[];         \
     strcatf(log_asmbuf, __VA_ARGS__); \
   } while (0)
 #else
