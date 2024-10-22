ac36d5aa03779793426d695880ab1fe798b403a9
tracer-ics2020 <tracer@njuics.org>
2024-10-22 21:03:10 +0800

    >  compile 191220000  Static hostname: DESKTOP-75S465I        Icon name: computer-container          Chassis: container       Machine ID: 1c9f91d336034cdebf733748cf9de30a          Boot ID: 418c0b9120264257bbe1d545106eb0c9   Virtualization: wsl Operating System: Ubuntu 22.04.5 LTS           Kernel: Linux 5.15.153.1-microsoft-standard-WSL2     Architecture: x86-64  21:03:10 up  7:17,  1 user,  load average: 0.27, 0.19, 0.09

--- nemu/src/isa/x86/local-include/reg.h
+++ nemu/src/isa/x86/local-include/reg.h
@@ -3,11 +3,42 @@
 
 #include <isa.h>
 
-enum { R_EAX, R_ECX, R_EDX, R_EBX, R_ESP, R_EBP, R_ESI, R_EDI };
-enum { R_AX, R_CX, R_DX, R_BX, R_SP, R_BP, R_SI, R_DI };
-enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
+enum
+{
+  R_EAX,
+  R_ECX,
+  R_EDX,
+  R_EBX,
+  R_ESP,
+  R_EBP,
+  R_ESI,
+  R_EDI
+};
+enum
+{
+  R_AX,
+  R_CX,
+  R_DX,
+  R_BX,
+  R_SP,
+  R_BP,
+  R_SI,
+  R_DI
+};
+enum
+{
+  R_AL,
+  R_CL,
+  R_DL,
+  R_BL,
+  R_AH,
+  R_CH,
+  R_DH,
+  R_BH
+};
 
-static inline int check_reg_index(int index) {
+static inline int check_reg_index(int index)
+{
   assert(index >= 0 && index < 8);
   return index;
 }
@@ -15,18 +46,24 @@ static inline int check_reg_index(int index) {
 #define reg_l(index) (cpu.gpr[check_reg_index(index)]._32)
 #define reg_w(index) (cpu.gpr[check_reg_index(index)]._16)
 #define reg_b(index) (cpu.gpr[check_reg_index(index) & 0x3]._8[index >> 2])
-
-static inline const char* reg_name(int index, int width) {
-  extern const char* regsl[];
-  extern const char* regsw[];
-  extern const char* regsb[];
+#define eflag(flag) (cpu.EFLAGS.flag)
+static inline const char *reg_name(int index, int width)
+{
+  extern const char *regsl[];
+  extern const char *regsw[];
+  extern const char *regsb[];
   assert(index >= 0 && index < 8);
 
-  switch (width) {
-    case 4: return regsl[index];
-    case 1: return regsb[index];
-    case 2: return regsw[index];
-    default: assert(0);
+  switch (width)
+  {
+  case 4:
+    return regsl[index];
+  case 1:
+    return regsb[index];
+  case 2:
+    return regsw[index];
+  default:
+    assert(0);
   }
 }
 
--- nemu/src/isa/x86/local-include/rtl.h
+++ nemu/src/isa/x86/local-include/rtl.h
@@ -163,11 +163,12 @@ static inline def_rtl(is_add_carry, rtlreg_t *dest,
 #define def_rtl_setget_eflags(f)                              \
   static inline def_rtl(concat(set_, f), const rtlreg_t *src) \
   {                                                           \
-    TODO();                                                   \
+    assert(*src == 0 || *src == 1);                           \
+    eflag(f) = *src;                                          \
   }                                                           \
   static inline def_rtl(concat(get_, f), rtlreg_t *dest)      \
   {                                                           \
-    TODO();                                                   \
+    *dest = eflag(f);                                         \
   }
 
 def_rtl_setget_eflags(CF)
