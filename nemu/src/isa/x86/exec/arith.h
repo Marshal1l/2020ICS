fb409a6796b573f482dc8dbdcefd8bc241298383
tracer-ics2020 <tracer@njuics.org>
2024-10-23 19:13:15 +0800

    >  run 191220000  Static hostname: DESKTOP-75S465I        Icon name: computer-container          Chassis: container       Machine ID: 1c9f91d336034cdebf733748cf9de30a          Boot ID: f0e71698cac2465f8122ea31a7c54c1c   Virtualization: wsl Operating System: Ubuntu 22.04.5 LTS           Kernel: Linux 5.15.153.1-microsoft-standard-WSL2     Architecture: x86-64  19:13:15 up  1:50,  1 user,  load average: 0.90, 0.34, 0.12

--- nemu/src/isa/x86/exec/arith.h
+++ nemu/src/isa/x86/exec/arith.h
@@ -1,13 +1,21 @@
 #include "cc.h"
 
-static inline def_EHelper(add)
-{
-  TODO();
+static inline def_EHelper(add) {
+  // TODO();
+  rtl_mv(s, t0, dsrc1);
+  rtl_mv(s, t1, ddest);
+  rtl_add(s, ddest, t1, t0);
+  rtl_is_add_overflow(s, s0, ddest, t1, t0, id_dest->width);
+  rtl_set_OF(s, s0);
+  rtl_is_add_carry(s, s0, ddest, t1);
+  rtl_set_CF(s, s0);
+  rtl_update_ZFSF(s, ddest, id_dest->width);
+  operand_write(s, id_dest, ddest);
   print_asm_template2(add);
 }
 
-static inline def_EHelper(sub)
-{
+static inline def_EHelper(sub) {
+  // TODO();
   rtl_mv(s, t0, dsrc1);
   rtl_mv(s, t1, ddest);
   rtl_sub(s, ddest, t1, t0);
@@ -20,40 +28,68 @@ static inline def_EHelper(sub)
   print_asm_template2(sub);
 }
 
-static inline def_EHelper(cmp)
-{
-  TODO();
+static inline def_EHelper(cmp) {
+  // TODO();
+  rtl_mv(s, t0, ddest);
+  rtl_mv(s, t1, dsrc1);
+  if (id_dest->width > 1)
+    rtl_sext(s, t1, dsrc1, id_src1->width);
+  rtl_sub(s, s0, t0, t1);
+  rtl_is_sub_carry(s, s1, t0, t1);
+  rtl_set_CF(s, s1);
+  rtl_is_sub_overflow(s, s1, s0, t0, t1, id_dest->width);
+  rtl_set_OF(s, s1);
+  rtl_update_ZFSF(s, s0, id_dest->width);
   print_asm_template2(cmp);
 }
 
-static inline def_EHelper(inc)
-{
-  TODO();
+static inline def_EHelper(inc) {
+  // TODO();
+  rtl_mv(s, t0, ddest);
+  rtl_li(s, t1, 1);
+  rtl_add(s, ddest, t0, t1);
+  rtl_is_add_overflow(s, s0, ddest, t0, t1, id_dest->width);
+  rtl_set_OF(s, s0);
+  rtl_update_ZFSF(s, ddest, id_dest->width);
+  operand_write(s, id_dest, ddest);
   print_asm_template1(inc);
 }
 
-static inline def_EHelper(dec)
-{
-  TODO();
+static inline def_EHelper(dec) {
+  // TODO();
+  rtl_mv(s, t0, ddest);
+  rtl_li(s, t1, 1);
+  rtl_sub(s, ddest, t0, t1);
+  rtl_is_add_overflow(s, s0, ddest, t0, t1, id_dest->width);
+  rtl_set_OF(s, s0);
+  rtl_update_ZFSF(s, ddest, id_dest->width);
+  operand_write(s, id_dest, ddest);
   print_asm_template1(dec);
 }
 
-static inline def_EHelper(neg)
-{
-  TODO();
+static inline def_EHelper(neg) {
+  // TODO();
+  if (id_dest->val == 0) {
+    rtl_li(s, s0, 0);
+    rtl_set_CF(s, s0);
+  } else {
+    rtl_li(s, s0, 1);
+    rtl_set_CF(s, s0);
+  }
+  rtl_neg(s, ddest, ddest);
+  rtl_update_ZFSF(s, ddest, id_dest->width);
+  operand_write(s, id_dest, ddest);
   print_asm_template1(neg);
 }
 
-static inline def_EHelper(adc)
-{
+static inline def_EHelper(adc) {
   rtl_get_CF(s, s0);
   rtl_add(s, s0, dsrc1, s0);
   rtl_add(s, s1, ddest, s0);
   rtl_update_ZFSF(s, s1, id_dest->width);
   rtl_is_add_overflow(s, s2, s1, ddest, dsrc1, id_dest->width);
   rtl_set_OF(s, s2);
-  if (id_dest->width != 4)
-  {
+  if (id_dest->width != 4) {
     rtl_andi(s, s1, s1, 0xffffffffu >> ((4 - id_dest->width) * 8));
   }
   rtl_is_add_carry(s, s2, s1, s0);
@@ -64,8 +100,7 @@ static inline def_EHelper(adc)
   print_asm_template2(adc);
 }
 
-static inline def_EHelper(sbb)
-{
+static inline def_EHelper(sbb) {
   rtl_get_CF(s, s0);
   rtl_add(s, s0, dsrc1, s0);
   rtl_sub(s, s1, ddest, s0);
@@ -80,70 +115,62 @@ static inline def_EHelper(sbb)
   print_asm_template2(sbb);
 }
 
-static inline def_EHelper(mul)
-{
+static inline def_EHelper(mul) {
   rtl_lr(s, s0, R_EAX, id_dest->width);
   rtl_mul_lo(s, s1, ddest, s0);
 
-  switch (id_dest->width)
-  {
-  case 1:
-    rtl_sr(s, R_AX, s1, 2);
-    break;
-  case 2:
-    rtl_sr(s, R_AX, s1, 2);
-    rtl_shri(s, s1, s1, 16);
-    rtl_sr(s, R_DX, s1, 2);
-    break;
-  case 4:
-    rtl_mul_hi(s, s0, ddest, s0);
-    rtl_sr(s, R_EDX, s0, 4);
-    rtl_sr(s, R_EAX, s1, 4);
-    break;
-  default:
-    assert(0);
+  switch (id_dest->width) {
+    case 1:
+      rtl_sr(s, R_AX, s1, 2);
+      break;
+    case 2:
+      rtl_sr(s, R_AX, s1, 2);
+      rtl_shri(s, s1, s1, 16);
+      rtl_sr(s, R_DX, s1, 2);
+      break;
+    case 4:
+      rtl_mul_hi(s, s0, ddest, s0);
+      rtl_sr(s, R_EDX, s0, 4);
+      rtl_sr(s, R_EAX, s1, 4);
+      break;
+    default: assert(0);
   }
 
   print_asm_template1(mul);
 }
 
 // imul with one operand
-static inline def_EHelper(imul1)
-{
-  switch (id_dest->width)
-  {
-  case 1:
-    rtl_lr(s, s0, R_EAX, 1);
-    rtl_imul_lo(s, s1, ddest, s0);
-    rtl_sr(s, R_AX, s1, 2);
-    break;
-  case 2:
-    rtl_lr(s, s0, R_EAX, 2);
-    rtl_imul_lo(s, s1, ddest, s0);
-    rtl_sr(s, R_AX, s1, 2);
-    rtl_shri(s, s1, s1, 16);
-    rtl_sr(s, R_DX, s1, 2);
-    break;
-  case 4:;
-    rtlreg_t *pdest = ddest;
-    if (ddest == &cpu.edx)
-    {
-      rtl_mv(s, s0, ddest);
-      pdest = s0;
-    }
-    rtl_imul_hi(s, &cpu.edx, pdest, &cpu.eax);
-    rtl_imul_lo(s, &cpu.eax, pdest, &cpu.eax);
-    break;
-  default:
-    assert(0);
+static inline def_EHelper(imul1) {
+  switch (id_dest->width) {
+    case 1:
+      rtl_lr(s, s0, R_EAX, 1);
+      rtl_imul_lo(s, s1, ddest, s0);
+      rtl_sr(s, R_AX, s1, 2);
+      break;
+    case 2:
+      rtl_lr(s, s0, R_EAX, 2);
+      rtl_imul_lo(s, s1, ddest, s0);
+      rtl_sr(s, R_AX, s1, 2);
+      rtl_shri(s, s1, s1, 16);
+      rtl_sr(s, R_DX, s1, 2);
+      break;
+    case 4:
+      ; rtlreg_t *pdest = ddest;
+      if (ddest == &cpu.edx) {
+        rtl_mv(s, s0, ddest);
+        pdest = s0;
+      }
+      rtl_imul_hi(s, &cpu.edx, pdest, &cpu.eax);
+      rtl_imul_lo(s, &cpu.eax, pdest, &cpu.eax);
+      break;
+    default: assert(0);
   }
 
   print_asm_template1(imul);
 }
 
 // imul with two operands
-static inline def_EHelper(imul2)
-{
+static inline def_EHelper(imul2) {
   rtl_sext(s, dsrc1, dsrc1, id_src1->width);
   rtl_sext(s, ddest, ddest, id_dest->width);
 
@@ -154,10 +181,9 @@ static inline def_EHelper(imul2)
 }
 
 // imul with three operands
-static inline def_EHelper(imul3)
-{
-  rtl_sext(s, dsrc1, dsrc1, id_dest->width);
-  rtl_sext(s, dsrc2, dsrc2, id_dest->width);
+static inline def_EHelper(imul3) {
+  rtl_sext(s, dsrc1, dsrc1, id_src1->width);
+  rtl_sext(s, dsrc2, dsrc2, id_src1->width);
 
   rtl_imul_lo(s, ddest, dsrc2, dsrc1);
   operand_write(s, id_dest, ddest);
@@ -165,73 +191,65 @@ static inline def_EHelper(imul3)
   print_asm_template3(imul);
 }
 
-static inline def_EHelper(div)
-{
-  switch (id_dest->width)
-  {
-  case 1:
-    rtl_lr(s, s0, R_AX, 2);
-    rtl_div_q(s, s1, s0, ddest);
-    rtl_sr(s, R_AL, s1, 1);
-    rtl_div_r(s, s1, s0, ddest);
-    rtl_sr(s, R_AH, s1, 1);
-    break;
-  case 2:
-    rtl_lr(s, s0, R_AX, 2);
-    rtl_lr(s, s1, R_DX, 2);
-    rtl_shli(s, s1, s1, 16);
-    rtl_or(s, s0, s0, s1);
-    rtl_div_q(s, s1, s0, ddest);
-    rtl_sr(s, R_AX, s1, 2);
-    rtl_div_r(s, s1, s0, ddest);
-    rtl_sr(s, R_DX, s1, 2);
-    break;
-  case 4:;
-    rtlreg_t *pdest = ddest;
-    if (ddest == &cpu.eax)
-      pdest = s0;
-    rtl_mv(s, s0, &cpu.eax);
-    rtl_div64_q(s, &cpu.eax, &cpu.edx, s0, pdest);
-    rtl_div64_r(s, &cpu.edx, &cpu.edx, s0, pdest);
-    break;
-  default:
-    assert(0);
+static inline def_EHelper(div) {
+  switch (id_dest->width) {
+    case 1:
+      rtl_lr(s, s0, R_AX, 2);
+      rtl_div_q(s, s1, s0, ddest);
+      rtl_sr(s, R_AL, s1, 1);
+      rtl_div_r(s, s1, s0, ddest);
+      rtl_sr(s, R_AH, s1, 1);
+      break;
+    case 2:
+      rtl_lr(s, s0, R_AX, 2);
+      rtl_lr(s, s1, R_DX, 2);
+      rtl_shli(s, s1, s1, 16);
+      rtl_or(s, s0, s0, s1);
+      rtl_div_q(s, s1, s0, ddest);
+      rtl_sr(s, R_AX, s1, 2);
+      rtl_div_r(s, s1, s0, ddest);
+      rtl_sr(s, R_DX, s1, 2);
+      break;
+    case 4:
+      ; rtlreg_t *pdest = ddest;
+      if (ddest == &cpu.eax) pdest = s0;
+      rtl_mv(s, s0, &cpu.eax);
+      rtl_div64_q(s, &cpu.eax, &cpu.edx, s0, pdest);
+      rtl_div64_r(s, &cpu.edx, &cpu.edx, s0, pdest);
+      break;
+    default: assert(0);
   }
 
   print_asm_template1(div);
 }
 
-static inline def_EHelper(idiv)
-{
-  switch (id_dest->width)
-  {
-  case 1:
-    rtl_lr(s, s0, R_AX, 2);
-    rtl_idiv_q(s, s1, s0, ddest);
-    rtl_sr(s, R_AL, s1, 1);
-    rtl_idiv_r(s, s1, s0, ddest);
-    rtl_sr(s, R_AH, s1, 1);
-    break;
-  case 2:
-    rtl_lr(s, s0, R_AX, 2);
-    rtl_lr(s, s1, R_DX, 2);
-    rtl_shli(s, s1, s1, 16);
-    rtl_or(s, s0, s0, s1);
-    rtl_idiv_q(s, s1, s0, ddest);
-    rtl_sr(s, R_AX, s1, 2);
-    rtl_idiv_r(s, s1, s0, ddest);
-    rtl_sr(s, R_DX, s1, 2);
-    break;
-  case 4:;
-    rtlreg_t *pdest = ddest;
-    if (ddest == &cpu.eax)
-      pdest = s0;
-    rtl_mv(s, s0, &cpu.eax);
-    rtl_idiv64_q(s, &cpu.eax, &cpu.edx, s0, pdest);
-    rtl_idiv64_r(s, &cpu.edx, &cpu.edx, s0, pdest);
-    break;
-  default:
-    assert(0);
+static inline def_EHelper(idiv) {
+  switch (id_dest->width) {
+    case 1:
+      rtl_lr(s, s0, R_AX, 2);
+      rtl_idiv_q(s, s1, s0, ddest);
+      rtl_sr(s, R_AL, s1, 1);
+      rtl_idiv_r(s, s1, s0, ddest);
+      rtl_sr(s, R_AH, s1, 1);
+      break;
+    case 2:
+      rtl_lr(s, s0, R_AX, 2);
+      rtl_lr(s, s1, R_DX, 2);
+      rtl_shli(s, s1, s1, 16);
+      rtl_or(s, s0, s0, s1);
+      rtl_idiv_q(s, s1, s0, ddest);
+      rtl_sr(s, R_AX, s1, 2);
+      rtl_idiv_r(s, s1, s0, ddest);
+      rtl_sr(s, R_DX, s1, 2);
+      break;
+    case 4:
+      ; rtlreg_t *pdest = ddest;
+      if (ddest == &cpu.eax) pdest = s0;
+      rtl_mv(s, s0, &cpu.eax);
+      rtl_idiv64_q(s, &cpu.eax, &cpu.edx, s0, pdest);
+      rtl_idiv64_r(s, &cpu.edx, &cpu.edx, s0, pdest);
+      break;
+    default: assert(0);
   }
 
   print_asm_template1(idiv);
--- nemu/src/isa/x86/exec/control.h
+++ nemu/src/isa/x86/exec/control.h
@@ -1,15 +1,13 @@
 #include "cc.h"
 
-static inline def_EHelper(jmp)
-{
+static inline def_EHelper(jmp) {
   // the target address is calculated at the decode stage
   rtl_j(s, s->jmp_pc);
 
   print_asm("jmp %x", s->jmp_pc);
 }
 
-static inline def_EHelper(jcc)
-{
+static inline def_EHelper(jcc) {
   // the target address is calculated at the decode stage
   uint32_t cc = s->opcode & 0xf;
   rtl_setcc(s, s0, cc);
@@ -18,43 +16,39 @@ static inline def_EHelper(jcc)
   print_asm("j%s %x", get_cc_name(cc), s->jmp_pc);
 }
 
-static inline def_EHelper(jmp_rm)
-{
+static inline def_EHelper(jmp_rm) {
   rtl_jr(s, ddest);
+
   print_asm("jmp *%s", id_dest->str);
 }
 
-static inline def_EHelper(call)
-{
+static inline def_EHelper(call) {
   // the target address is calculated at the decode stage
+  // TODO();
   rtl_push(s, &s->seq_pc);
-  rtl_j(s, s->jmp_pc);
+  rtl_jr(s, &s->jmp_pc);
   print_asm("call %x", s->jmp_pc);
 }
 
-static inline def_EHelper(ret)
-{
-  if (s->isa.is_operand_size_16 == true)
-  {
+static inline def_EHelper(ret) {
+  // TODO();
+  if (s->isa.is_operand_size_16 == true) {
     rtl_pop(s, &s->seq_pc);
     s->seq_pc &= 0x0000FFFF;
-  }
-  else
-  {
+  } else {
     rtl_pop(s, &s->seq_pc);
   }
   print_asm("ret");
-  print_asm("(%x)", s->seq_pc);
 }
 
-static inline def_EHelper(ret_imm)
-{
+static inline def_EHelper(ret_imm) {
   TODO();
   print_asm("ret %s", id_dest->str);
 }
 
-static inline def_EHelper(call_rm)
-{
-  TODO();
+static inline def_EHelper(call_rm) {
+  // TODO();
+  rtl_push(s, &s->seq_pc);
+  rtl_jr(s, ddest);
   print_asm("call *%s", id_dest->str);
 }
