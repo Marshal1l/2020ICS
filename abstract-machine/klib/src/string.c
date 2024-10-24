08f3af20a27e437837827eff316c0b3ed0414fdc
tracer-ics2020 <tracer@njuics.org>
2024-10-24 20:00:22 +0800

    >  run 191220000  Static hostname: DESKTOP-75S465I        Icon name: computer-container          Chassis: container       Machine ID: 1c9f91d336034cdebf733748cf9de30a          Boot ID: f0e71698cac2465f8122ea31a7c54c1c   Virtualization: wsl Operating System: Ubuntu 22.04.5 LTS           Kernel: Linux 5.15.153.1-microsoft-standard-WSL2     Architecture: x86-64  20:00:22 up 1 day,  2:36,  1 user,  load average: 0.25, 0.28, 0.18

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -2,102 +2,102 @@
 #include <stdint.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
-
+//
 size_t strlen(const char *s)
-{ // tested
-  const char *char_ptr = s;
-  while (*char_ptr != '\0')
-    char_ptr++;
-  return char_ptr - s;
+{
+  const char *tmp = s;
+  while (*s != '\0')
+  {
+    s++;
+  }
+  return tmp - s;
 }
-
+//
 char *strcpy(char *dst, const char *src)
-{ // tested
-  return memcpy((void *)dst, (const void *)src, strlen(src) + 1);
+{
+  return memcpy((void *)dst, (void *)src, strlen(src) + 1);
 }
 
 char *strncpy(char *dst, const char *src, size_t n)
-{ // tested
+{
   size_t i;
-  for (i = 0; i < n && src[i] != '\0'; ++i)
-  {
+  for (i = 0; i < n && src[i] != '\0'; i++)
     dst[i] = src[i];
-  }
-  for (; i < n; ++i)
-  {
+  for (; i < n; i++)
     dst[i] = '\0';
-  }
   return dst;
 }
 
 char *strcat(char *dst, const char *src)
-{ // tested
-  strcpy(dst + strlen(dst), src);
-  return dst;
+{
+  return strcpy(dst + strlen(dst), src);
 }
-
+// strcmp
 int strcmp(const char *s1, const char *s2)
-{ // tested
-  const char *p1 = s1;
-  const char *p2 = s2;
+{
+  const char *tmp1 = s1;
+  const char *tmp2 = s2;
   char a, b;
   do
   {
-    a = *p1++;
-    b = *p2++;
-    if (a == '\0')
-    {
+    a = *tmp1++;
+    b = *tmp2++;
+    if (a == '\0' || b == '\0')
       return a - b;
-    }
   } while (a == b);
   return a - b;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n)
-{ // tested
-  uint8_t c1 = '\0';
-  uint8_t c2 = '\0';
-  while (n > 0)
+{
+  const char *tmp1 = s1;
+  const char *tmp2 = s2;
+  char a, b;
+  do
   {
-    c1 = (uint8_t)*s1++;
-    c2 = (uint8_t)*s2++;
-    if (c1 == '\0' || c1 != c2)
-    {
-      return c1 - c2;
-    }
+    a = *tmp1++;
+    b = *tmp2++;
     n--;
-  }
-  return c1 - c2;
+    if (a == '\0' || b == '\0' || n == 0)
+      return a - b;
+  } while (a == b);
+  return a - b;
 }
 
 void *memset(void *v, int c, size_t n)
-{ // tested
+{
   uint8_t *dst = (uint8_t *)v;
   while (n > 0)
   {
-    dst[0] = c;
-    dst++;
+    *dst++ = c;
     n--;
   }
   return v;
 }
-
+// 防止内存重叠
 void *memmove(void *dst, const void *src, size_t n)
-{ // tested
-  uint8_t *c1 = (uint8_t *)dst;
-  uint8_t *c2 = (uint8_t *)src;
-  while (n > 0)
+{
+  uint8_t *tmpsrc = (uint8_t *)src;
+  uint8_t *tmpdst = (uint8_t *)dst;
+  if (dst < src)
   {
-    *c1 = *c2;
-    c1++;
-    c2++;
-    n--;
+    for (size_t i = 0; i < n; ++i)
+    {
+      tmpdst[i] = tmpsrc[i];
+    }
+  }
+  else
+  {
+    for (size_t i = n - 1; i > -1; --i)
+    {
+      tmpdst[i] = tmpdst[i];
+    }
   }
   return dst;
 }
-
+//
 void *memcpy(void *out, const void *in, size_t n)
-{ // tested
+{
   uint8_t *dest = (uint8_t *)out;
   uint8_t *src = (uint8_t *)in;
   size_t i;
@@ -109,24 +109,19 @@ void *memcpy(void *out, const void *in, size_t n)
 }
 
 int memcmp(const void *s1, const void *s2, size_t n)
-{                             // tested
-  uint8_t *a = (uint8_t *)s1; // I don't know why the result in macOS is different from the result in linux
-  uint8_t *b = (uint8_t *)s2;
-  int32_t a0, b0, res;
-  while (n > 0)
+{
+  uint8_t *tmp1 = (uint8_t *)s1;
+  uint8_t *tmp2 = (uint8_t *)s2;
+  uint8_t a, b;
+  do
   {
-    a0 = ((uint8_t *)a)[0];
-    b0 = ((uint8_t *)b)[0];
-    a++;
-    b++;
-    res = a0 - b0;
-    if (res != 0)
-    {
-      return res;
-    }
+    a = *tmp1++;
+    b = *tmp2++;
     n--;
-  }
-  return 0;
+    if (a == '\0' || n == 0)
+      return a - b;
+  } while (a == b);
+  return a - b;
 }
 
 #endif
