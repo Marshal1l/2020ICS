1d3682bc4a1ab8035a68d5706bf5a122b094e202
tracer-ics2020 <tracer@njuics.org>
2024-10-26 21:00:05 +0800

    >  run 191220000  Static hostname: DESKTOP-75S465I        Icon name: computer-container          Chassis: container       Machine ID: 1c9f91d336034cdebf733748cf9de30a          Boot ID: 152cccf1026a4e3b98694701e2b6b494   Virtualization: wsl Operating System: Ubuntu 22.04.5 LTS           Kernel: Linux 5.15.153.1-microsoft-standard-WSL2     Architecture: x86-64  21:00:05 up 1 day,  2:54,  1 user,  load average: 0.18, 0.15, 0.10

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,23 +5,63 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char *fmt, ...) {
+#define MAX_BUF 2048
+
+#define is_digit(ch) ((ch) >= '0' && (ch) <= '9')
+
+int vsprintf(char *out, const char *fmt, va_list ap)
+{
+  char *buf_ptr = out;
+  const char *fmt_ptr = fmt;
+
+  while (*fmt_ptr != '\0')
+  {
+    if (*fmt_ptr == '%' && *(fmt_ptr + 1) != '\0')
+    {
+      fmt_ptr++;
+      switch (*fmt_ptr)
+      {
+      case 'd':
+      {
+        int i = va_arg(ap, int);
+        *(int *)buf_ptr = i;
+        buf_ptr += sizeof(int);
+        break;
+      }
+      case 's':
+      {
+        char *t_str = va_arg(ap, char *);
+        for (int start = 0; t_str[start] != '\0'; t_str++)
+        {
+          *(char *)buf_ptr = t_str[start];
+          buf_ptr += sizeof(char);
+        }
+        break;
+      }
+      default:
+        break;
+      }
+    }
+  }
   return 0;
 }
 
-int vsprintf(char *out, const char *fmt, va_list ap) {
+int sprintf(char *out, const char *fmt, ...)
+{
+  va_list args;
+  va_start(args, fmt);
+  vsprintf(out, fmt, args);
+  va_end(args);
   return 0;
 }
 
-int sprintf(char *out, const char *fmt, ...) {
+int snprintf(char *out, size_t n, const char *fmt, ...)
+{
   return 0;
 }
 
-int snprintf(char *out, size_t n, const char *fmt, ...) {
-  return 0;
-}
-
-int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
+int vsnprintf(char *out, size_t n, const char *fmt, va_list ap)
+{
   return 0;
 }
 
