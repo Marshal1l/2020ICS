25a37c97fa97ded095a20524f3eebbdfc4cd6160
tracer-ics2020 <tracer@njuics.org>
2024-10-27 19:14:04 +0800

    >  compile 191220000  Static hostname: DESKTOP-75S465I        Icon name: computer-container          Chassis: container       Machine ID: 1c9f91d336034cdebf733748cf9de30a          Boot ID: 1797dea516544bdc89c158d83ddb4a7c   Virtualization: wsl Operating System: Ubuntu 22.04.5 LTS           Kernel: Linux 5.15.153.1-microsoft-standard-WSL2     Architecture: x86-64  19:14:04 up  5:52,  1 user,  load average: 0.05, 0.14, 0.18

--- nemu/src/monitor/debug/log.c
+++ nemu/src/monitor/debug/log.c
@@ -41,7 +41,7 @@ void init_log(const char *log_file)
 void print_ring()
 {
   printf("Ring buffer list:\n");
-  for (Log_ring_node *tmp = head_node; (!if_emptynode(tmp)) && tmp->next != head_node; tmp = tmp->next)
+  for (Log_ring_node *tmp = head_node; (!if_emptynode(tmp)) && tmp != head_node; tmp = tmp->next)
   {
     printf("--%s\n", tmp->log_asm);
   }
