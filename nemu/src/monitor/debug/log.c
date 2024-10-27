c4e0621baf0c6839199a492d7d5e3a40b45a908e
tracer-ics2020 <tracer@njuics.org>
2024-10-27 18:54:52 +0800

    >  compile 191220000  Static hostname: DESKTOP-75S465I        Icon name: computer-container          Chassis: container       Machine ID: 1c9f91d336034cdebf733748cf9de30a          Boot ID: 1797dea516544bdc89c158d83ddb4a7c   Virtualization: wsl Operating System: Ubuntu 22.04.5 LTS           Kernel: Linux 5.15.153.1-microsoft-standard-WSL2     Architecture: x86-64  18:54:52 up  5:32,  1 user,  load average: 0.30, 0.31, 0.27

--- nemu/src/monitor/debug/log.c
+++ nemu/src/monitor/debug/log.c
@@ -1,19 +1,59 @@
 #include <common.h>
 #include <stdarg.h>
-
+#define RING_SIZE 20
 FILE *log_fp = NULL;
-
-void init_log(const char *log_file) {
-  if (log_file == NULL) return;
-  log_fp = fopen(log_file, "w");
-  Assert(log_fp, "Can not open '%s'", log_file);
-}
+typedef struct Log_ring_node Log_ring_node;
+typedef struct Log_ring_node
+{
+  char log_asm[80];
+  Log_ring_node *next;
+} Log_ring_node;
 
 char log_bytebuf[80] = {};
 char log_asmbuf[80] = {};
 static char tempbuf[256] = {};
+static Log_ring_node log_ring[RING_SIZE];
+static Log_ring_node *head_node = &log_ring[0];
+static Log_ring_node *free_node = &log_ring[0];
+int if_emptynode(Log_ring_node *node)
+{
+  if (node->log_asm[0] == '\0')
+  {
+    return 1;
+  }
+  return 0;
+}
+void init_log(const char *log_file)
+{
+  if (log_file == NULL)
+    return;
+  log_fp = fopen(log_file, "w");
+  Assert(log_fp, "Can not open '%s'", log_file);
+  free_node = log_ring;
+  // 连接环形链表
+  for (int i = 0; i < RING_SIZE - 1; i++)
+  {
+    log_ring[i].next = log_ring[i + 1].next;
+  }
+  log_ring[RING_SIZE - 1].next = &log_ring[0];
+  head_node = &log_ring[0];
+}
 
-void strcatf(char *buf, const char *fmt, ...) {
+void add2ring(const char *fmt, ...)
+{
+  va_list ap;
+  va_start(ap, fmt);
+  vsnprintf(tempbuf, sizeof(tempbuf), fmt, ap);
+  va_end(ap);
+  if (!if_emptynode(free_node))
+  {
+    head_node = head_node->next;
+  }
+  strcat(free_node->log_asm, tempbuf);
+  free_node = free_node->next;
+}
+void strcatf(char *buf, const char *fmt, ...)
+{
   va_list ap;
   va_start(ap, fmt);
   vsnprintf(tempbuf, sizeof(tempbuf), fmt, ap);
@@ -21,11 +61,13 @@ void strcatf(char *buf, const char *fmt, ...) {
   strcat(buf, tempbuf);
 }
 
-void asm_print(vaddr_t this_pc, int instr_len, bool print_flag) {
+void asm_print(vaddr_t this_pc, int instr_len, bool print_flag)
+{
   snprintf(tempbuf, sizeof(tempbuf), FMT_WORD ":   %s%*.s%s", this_pc, log_bytebuf,
-      50 - (12 + 3 * instr_len), "", log_asmbuf);
+           50 - (12 + 3 * instr_len), "", log_asmbuf);
   log_write("%s\n", tempbuf);
-  if (print_flag) {
+  if (print_flag)
+  {
     puts(tempbuf);
   }
 
