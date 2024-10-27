9bea55668dbc9db588514dc773e84b40cb76b6f0
tracer-ics2020 <tracer@njuics.org>
2024-10-27 13:45:31 +0800

    >  run 191220000  Static hostname: DESKTOP-75S465I        Icon name: computer-container          Chassis: container       Machine ID: 1c9f91d336034cdebf733748cf9de30a          Boot ID: 1797dea516544bdc89c158d83ddb4a7c   Virtualization: wsl Operating System: Ubuntu 22.04.5 LTS           Kernel: Linux 5.15.153.1-microsoft-standard-WSL2     Architecture: x86-64  13:45:31 up 23 min,  1 user,  load average: 0.15, 0.14, 0.08

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -6,15 +6,49 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 #define MAX_BUF 2048
-
+#define TMP_INT_LEN 20
 #define is_digit(ch) ((ch) >= '0' && (ch) <= '9')
-
+char tmp_int[TMP_INT_LEN];
+// buf char len
+int buf_len = 0;
+// 1 for ok 0 for out
+int check_len(int len)
+{
+  if (len > MAX_BUF)
+  {
+    return 0;
+  }
+  else
+  {
+    return 1;
+  }
+}
+int i2s(int num)
+{
+  // reset tmp_int
+  for (int i = 0; i < TMP_INT_LEN; i++)
+  {
+    tmp_int[i] = '\0';
+  }
+  int single = 0;
+  int int_count = 0;
+  while (num != 0)
+  {
+    if (int_count >= TMP_INT_LEN)
+      return 0;
+    single = num % 10;
+    tmp_int[int_count++] = '0' + single;
+    num = num / 10;
+  }
+  return 1;
+}
 int vsprintf(char *out, const char *fmt, va_list ap)
 {
   char *buf_ptr = out;
   const char *fmt_ptr = fmt;
+  int print_count = 0;
   // putstr("into vsprintf\n");
-  while (*fmt_ptr != '\0')
+  while ((*fmt_ptr) != '\0')
   {
     if (*fmt_ptr == '%' && *(fmt_ptr + 1) != '\0')
     {
@@ -24,18 +58,35 @@ int vsprintf(char *out, const char *fmt, va_list ap)
       case 'd':
       {
         int i = va_arg(ap, int);
-        *(int *)buf_ptr = i;
-        buf_ptr += sizeof(int);
+        // check if buffer out of range
+        if (!check_len(buf_len))
+          return -1;
+        // fommat int->s then put in
+        if (!i2s(i))
+          return -1;
+        // var
+        int index = 0;
+        while (tmp_int[index] != '\0')
+        {
+          *(char *)buf_ptr = tmp_int[index];
+          buf_ptr += sizeof(char);
+          buf_len++;
+          print_count++;
+        }
         break;
       }
       case 's':
       {
         char *t_str = va_arg(ap, char *);
-        for (int start = 0; t_str[start] != '\0'; t_str++)
+        int start = 0;
+        for (; t_str[start] != '\0'; t_str++)
         {
+          if (!check_len(buf_len))
+            return -1;
           *(char *)buf_ptr = t_str[start];
           buf_ptr += sizeof(char);
         }
+        buf_len += sizeof(char) * start;
         break;
       }
       default:
