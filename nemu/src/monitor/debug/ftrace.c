37978fcde1d823358261860d06fc714c44435287
tracer-ics2020 <tracer@njuics.org>
2024-10-27 21:08:04 +0800

    >  compile 191220000  Static hostname: DESKTOP-75S465I        Icon name: computer-container          Chassis: container       Machine ID: 1c9f91d336034cdebf733748cf9de30a          Boot ID: 1797dea516544bdc89c158d83ddb4a7c   Virtualization: wsl Operating System: Ubuntu 22.04.5 LTS           Kernel: Linux 5.15.153.1-microsoft-standard-WSL2     Architecture: x86-64  21:08:04 up  7:46,  1 user,  load average: 0.16, 0.19, 0.18

--- nemu/src/monitor/debug/ftrace.c
+++ nemu/src/monitor/debug/ftrace.c
@@ -1,6 +1,13 @@
 #include <common.h>
 #include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <elf.h>
 void init_ftrase(const char *elf_file)
 {
-    printf("init ftrace!\n");
+    int fd = open(elf_file, O_RDONLY);
+    close(fd);
 }
