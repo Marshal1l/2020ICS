103b3910a0b01829b73fa5067073b867c10844b2
tracer-ics2020 <tracer@njuics.org>
2024-10-28 22:02:58 +0800

    >  compile 191220000  Static hostname: DESKTOP-75S465I        Icon name: computer-container          Chassis: container       Machine ID: 1c9f91d336034cdebf733748cf9de30a          Boot ID: 1797dea516544bdc89c158d83ddb4a7c   Virtualization: wsl Operating System: Ubuntu 22.04.5 LTS           Kernel: Linux 5.15.153.1-microsoft-standard-WSL2     Architecture: x86-64  22:02:58 up 1 day,  7:35,  1 user,  load average: 0.24, 0.41, 0.34

--- nemu/src/monitor/debug/ftrace.c
+++ nemu/src/monitor/debug/ftrace.c
@@ -68,11 +68,10 @@ void init_ftrace(const char *elf_file)
                 printf("fail to read the strtab\n");
                 exit(0);
             }
-            printf("string table:%c\n", string_table[2]);
-            // for (int i = 0; i < shdr.sh_size / sizeof(char); i++)
-            // {
-            //     printf("string table:%c\n", string_table[i]);
-            // }
+            for (int i = 0; i < shdr.sh_size / sizeof(char); i++)
+            {
+                printf("string table:%c\n", string_table[i]);
+            }
         }
     }
 
