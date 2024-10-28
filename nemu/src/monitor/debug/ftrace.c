7fbccc6bcba4d88c7b0958d8f0dc348d790e7860
tracer-ics2020 <tracer@njuics.org>
2024-10-28 16:31:57 +0800

    >  compile 191220000  Static hostname: DESKTOP-75S465I        Icon name: computer-container          Chassis: container       Machine ID: 1c9f91d336034cdebf733748cf9de30a          Boot ID: 1797dea516544bdc89c158d83ddb4a7c   Virtualization: wsl Operating System: Ubuntu 22.04.5 LTS           Kernel: Linux 5.15.153.1-microsoft-standard-WSL2     Architecture: x86-64  16:31:57 up 1 day,  2:22,  1 user,  load average: 0.12, 0.19, 0.17

--- nemu/src/monitor/debug/ftrace.c
+++ nemu/src/monitor/debug/ftrace.c
@@ -12,14 +12,15 @@ void init_ftrase(const char *elf_file)
     {
         return;
     }
-    int fd = open(elf_file, O_RDONLY);
-    if (fd < 0)
+    FILE *fd;
+    fd = fopen(elf_file, "rb");
+    if (fd == NULL)
     {
         fprintf(stderr, "Usage: %s <elf-file>\n", elf_file);
         assert(0);
     }
     Elf64_Ehdr ehdr;
-    if (read(fd, &ehdr, sizeof(ehdr)) != sizeof(ehdr))
+    if (fread(&ehdr, sizeof(ehdr), 1, fd) != sizeof(ehdr))
     {
         fprintf(stderr, "read ehdr failed\n");
         assert(0);
@@ -28,12 +29,30 @@ void init_ftrase(const char *elf_file)
         ehdr.e_ident[2] != 'L' || ehdr.e_ident[3] != 'F')
     {
         printf("The opened file isn't a elf file!\n");
-        exit(0);
-    }
-    if (ehdr.e_shstrndx >= ehdr.e_shnum)
-    {
-        fprintf(stderr, "Invalid section header string table index\n");
         assert(0);
     }
-    close(fd);
+    fseek(fd, ehdr.e_shoff, SEEK_SET);
+    Elf64_Shdr shdr;
+    char *string_table = NULL;
+    for (int i = 0; i < ehdr.e_shnum; i++)
+    {
+        if (fread(&shdr, sizeof(Elf64_Shdr), 1, fd) <= 0)
+        {
+            printf("fail to read the shdr\n");
+            assert(0);
+        }
+        if (shdr.sh_type == SHT_STRTAB)
+        {
+            // get string table
+            string_table = malloc(shdr.sh_size);
+            fseek(fd, shdr.sh_offset, SEEK_SET);
+            if (fread(string_table, shdr.sh_size, 1, fd) <= 0)
+            {
+                printf("fail to read the strtab\n");
+                exit(0);
+            }
+        }
+    }
+
+    fclose(fd);
 }
