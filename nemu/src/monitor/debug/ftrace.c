62014b269c0a1211594af558058b5c34a2fb9721
tracer-ics2020 <tracer@njuics.org>
2024-10-28 17:14:29 +0800

    >  compile 191220000  Static hostname: DESKTOP-75S465I        Icon name: computer-container          Chassis: container       Machine ID: 1c9f91d336034cdebf733748cf9de30a          Boot ID: 1797dea516544bdc89c158d83ddb4a7c   Virtualization: wsl Operating System: Ubuntu 22.04.5 LTS           Kernel: Linux 5.15.153.1-microsoft-standard-WSL2     Architecture: x86-64  17:14:29 up 1 day,  3:02,  1 user,  load average: 0.00, 0.06, 0.12

--- nemu/src/monitor/debug/ftrace.c
+++ nemu/src/monitor/debug/ftrace.c
@@ -40,7 +40,7 @@ void init_ftrase(const char *elf_file)
         assert(0);
     }
     Elf64_Ehdr ehdr;
-    if (fread(&ehdr, sizeof(Elf64_Ehdr), 1, fd))
+    if (fread(&ehdr, sizeof(Elf64_Ehdr), 1, fd) == 1)
     {
         fprintf(stderr, "read ehdr failed\n");
         assert(0);
