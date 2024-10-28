a4e2191c66bf3ed93030caec1f37f46a8d7f691d
tracer-ics2020 <tracer@njuics.org>
2024-10-28 21:23:14 +0800

    >  compile 191220000  Static hostname: DESKTOP-75S465I        Icon name: computer-container          Chassis: container       Machine ID: 1c9f91d336034cdebf733748cf9de30a          Boot ID: 1797dea516544bdc89c158d83ddb4a7c   Virtualization: wsl Operating System: Ubuntu 22.04.5 LTS           Kernel: Linux 5.15.153.1-microsoft-standard-WSL2     Architecture: x86-64  21:23:14 up 1 day,  6:57,  1 user,  load average: 0.16, 0.20, 0.20

--- nemu/src/isa/x86/exec/control.h
+++ nemu/src/isa/x86/exec/control.h
@@ -29,7 +29,7 @@ static inline def_EHelper(call)
 {
   // the target address is calculated at the decode stage
   // TODO();
-  check_func_call(s->jmp_pc);
+  printf("%s", check_func_call(s->jmp_pc));
   rtl_push(s, &s->seq_pc);
   rtl_jr(s, &s->jmp_pc);
   print_asm("call %x", s->jmp_pc);
