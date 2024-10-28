9b6e1a24c40dc4fbcf66387724e68726daad4dd3
tracer-ics2020 <tracer@njuics.org>
2024-10-28 21:51:04 +0800

    >  compile 191220000  Static hostname: DESKTOP-75S465I        Icon name: computer-container          Chassis: container       Machine ID: 1c9f91d336034cdebf733748cf9de30a          Boot ID: 1797dea516544bdc89c158d83ddb4a7c   Virtualization: wsl Operating System: Ubuntu 22.04.5 LTS           Kernel: Linux 5.15.153.1-microsoft-standard-WSL2     Architecture: x86-64  21:51:04 up 1 day,  7:23,  1 user,  load average: 0.70, 0.43, 0.31

--- nemu/src/monitor/debug/ftrace.c
+++ nemu/src/monitor/debug/ftrace.c
@@ -68,8 +68,12 @@ void init_ftrace(const char *elf_file)
             }
         }
     }
-    printf("string table:%c\n", string_table[0]);
-    fflush(stdout);
+    for (int i = 0; i < shdr.sh_size; i++)
+    {
+        printf("string table:%c\n", string_table[0]);
+        fflush(stdout);
+    }
+
     pause();
     fseek(fd, ehdr.e_shoff, SEEK_SET);
     for (int i = 0; i < ehdr.e_shnum; i++)
