c55d3fcb447c4a32ddf67306170c522a0d73b8ca
tracer-ics2020 <tracer@njuics.org>
2024-10-29 21:00:36 +0800

    >  compile 191220000  Static hostname: DESKTOP-75S465I        Icon name: computer-container          Chassis: container       Machine ID: 1c9f91d336034cdebf733748cf9de30a          Boot ID: 1797dea516544bdc89c158d83ddb4a7c   Virtualization: wsl Operating System: Ubuntu 22.04.5 LTS           Kernel: Linux 5.15.153.1-microsoft-standard-WSL2     Architecture: x86-64  21:00:36 up 2 days,  5:56,  1 user,  load average: 0.04, 0.11, 0.13

--- nemu/src/isa/x86/exec/control.h
+++ nemu/src/isa/x86/exec/control.h
@@ -33,6 +33,10 @@ static inline def_EHelper(call)
   rtl_push(s, &s->seq_pc);
   rtl_jr(s, &s->jmp_pc);
   const char *tmp_cr = check_func_call(s->jmp_pc, 1);
+  if (!strcmp(tmp_cr, "???"))
+  {
+    assert(0);
+  }
   //++func_deepth;
   add_call_ret("call %s<%x>", tmp_cr, s->jmp_pc);
   print_asm("call %s<%x>", tmp_cr, s->jmp_pc);
@@ -52,6 +56,10 @@ static inline def_EHelper(ret)
   }
   //--func_deepth;
   const char *tmp_cr = check_func_call(s->seq_pc, 0);
+  if (!strcmp(tmp_cr, "???"))
+  {
+    assert(0);
+  }
   add_call_ret("ret %s<%x>", tmp_cr, s->seq_pc);
   print_asm("ret %s<%x>", tmp_cr, s->seq_pc);
 }
