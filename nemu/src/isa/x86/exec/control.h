ac166169ea4c74a8fc2cdf1244d789931da0b7d7
tracer-ics2020 <tracer@njuics.org>
2024-10-29 21:06:21 +0800

    >  compile 191220000  Static hostname: DESKTOP-75S465I        Icon name: computer-container          Chassis: container       Machine ID: 1c9f91d336034cdebf733748cf9de30a          Boot ID: 1797dea516544bdc89c158d83ddb4a7c   Virtualization: wsl Operating System: Ubuntu 22.04.5 LTS           Kernel: Linux 5.15.153.1-microsoft-standard-WSL2     Architecture: x86-64  21:06:21 up 2 days,  6:01,  1 user,  load average: 0.11, 0.08, 0.09

--- nemu/src/isa/x86/exec/control.h
+++ nemu/src/isa/x86/exec/control.h
@@ -75,7 +75,11 @@ static inline def_EHelper(call_rm)
   // TODO();
   rtl_push(s, &s->seq_pc);
   rtl_jr(s, ddest);
-  const char *tmp_cr = check_func_call(*id_dest->str, 1);
+  const char *tmp_cr = check_func_call(*ddest, 1);
+  if (!strcmp(tmp_cr, "???"))
+  {
+    assert(0);
+  }
   add_call_ret("call *%s<%x>", id_dest->str, tmp_cr);
   print_asm("call *%s<%x>", id_dest->str, tmp_cr);
 }
