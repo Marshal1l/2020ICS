ba66be1dded3179482300c2f40f3ee64766e59eb
tracer-ics2020 <tracer@njuics.org>
2024-10-29 21:16:12 +0800

    >  compile 191220000  Static hostname: DESKTOP-75S465I        Icon name: computer-container          Chassis: container       Machine ID: 1c9f91d336034cdebf733748cf9de30a          Boot ID: 1797dea516544bdc89c158d83ddb4a7c   Virtualization: wsl Operating System: Ubuntu 22.04.5 LTS           Kernel: Linux 5.15.153.1-microsoft-standard-WSL2     Architecture: x86-64  21:16:12 up 2 days,  6:11,  1 user,  load average: 0.11, 0.11, 0.09

--- nemu/src/isa/x86/exec/control.h
+++ nemu/src/isa/x86/exec/control.h
@@ -33,18 +33,16 @@ static inline def_EHelper(call)
   rtl_push(s, &s->seq_pc);
   rtl_jr(s, &s->jmp_pc);
   const char *tmp_cr = check_func_call(s->jmp_pc, 1);
-  if (!strcmp(tmp_cr, "???"))
-  {
-    assert(0);
-  }
-  //++func_deepth;
-  add_call_ret("call %s<%x>", tmp_cr, s->jmp_pc);
+  // if (!strcmp(tmp_cr, "???"))
+  // {
+  //   assert(0);
+  // }
+  // add_call_ret("call %s<%x>", tmp_cr, s->jmp_pc);
   print_asm("call %s<%x>", tmp_cr, s->jmp_pc);
 }
 
 static inline def_EHelper(ret)
 {
-  // TODO();
   if (s->isa.is_operand_size_16 == true)
   {
     rtl_pop(s, &s->seq_pc);
@@ -54,13 +52,12 @@ static inline def_EHelper(ret)
   {
     rtl_pop(s, &s->seq_pc);
   }
-  //--func_deepth;
   const char *tmp_cr = check_func_call(s->seq_pc, 0);
-  if (!strcmp(tmp_cr, "???"))
-  {
-    assert(0);
-  }
-  add_call_ret("ret %s<%x>", tmp_cr, s->seq_pc);
+  // if (!strcmp(tmp_cr, "???"))
+  // {
+  //   assert(0);
+  // }
+  // add_call_ret("ret %s<%x>", tmp_cr, s->seq_pc);
   print_asm("ret %s<%x>", tmp_cr, s->seq_pc);
 }
 
@@ -77,10 +74,10 @@ static inline def_EHelper(call_rm)
   rtl_jr(s, ddest);
 
   const char *tmp_cr = check_func_call((paddr_t)(*ddest), 1);
-  if (!strcmp(tmp_cr, "???"))
-  {
-    assert(0);
-  }
-  add_call_ret("call %s<%x>", tmp_cr, (paddr_t)(*ddest));
+  //  if (!strcmp(tmp_cr, "???"))
+  //  {
+  //    assert(0);
+  //  }
+  // add_call_ret("call %s<%x>", tmp_cr, (paddr_t)(*ddest));
   print_asm("call %s<%x>", id_dest->str, tmp_cr);
 }
