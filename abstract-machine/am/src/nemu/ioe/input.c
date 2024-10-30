3bdaf3d020ff5ddb00463d591dd8f45dfccc57a5
tracer-ics2020 <tracer@njuics.org>
2024-10-30 18:31:33 +0800

    >  compile 191220000  Static hostname: DESKTOP-75S465I        Icon name: computer-container          Chassis: container       Machine ID: 1c9f91d336034cdebf733748cf9de30a          Boot ID: 1797dea516544bdc89c158d83ddb4a7c   Virtualization: wsl Operating System: Ubuntu 22.04.5 LTS           Kernel: Linux 5.15.153.1-microsoft-standard-WSL2     Architecture: x86-64  18:31:33 up 3 days,  2:52,  1 user,  load average: 0.29, 0.13, 0.05

--- abstract-machine/am/src/nemu/ioe/input.c
+++ abstract-machine/am/src/nemu/ioe/input.c
@@ -3,7 +3,17 @@
 
 #define KEYDOWN_MASK 0x8000
 
-void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd)
+{
+  uint8_t rkeycode = -1;
+  if ((rkeycode = inl(KBD_ADDR)) != -1)
+  {
+    kbd->keydown = 1;
+    kbd->keycode = rkeycode;
+  }
+  else
+  {
+    kbd->keydown = 0;
+    kbd->keycode = AM_KEY_NONE;
+  }
 }
