58ce38ec427d3b1cc589528acc8995871ebf1ddc
tracer-ics2020 <tracer@njuics.org>
2024-10-31 22:05:25 +0800

    >  run 191220000  Static hostname: DESKTOP-75S465I        Icon name: computer-container          Chassis: container       Machine ID: 1c9f91d336034cdebf733748cf9de30a          Boot ID: 1797dea516544bdc89c158d83ddb4a7c   Virtualization: wsl Operating System: Ubuntu 22.04.5 LTS           Kernel: Linux 5.15.153.1-microsoft-standard-WSL2     Architecture: x86-64  22:05:26 up 4 days,  5:37,  1 user,  load average: 0.09, 0.11, 0.07

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -49,7 +49,7 @@ size_t fs_write(int fd, const void *buf, size_t len)
   size_t size = 0;
   if (fd == 1)
   {
-    for (int i = 0; i < len && *(const uint8_t *)buf; i++)
+    for (int i = 0; i < len && *(const uint8_t *)buf != '\0'; i++)
     {
       putch(*(const uint8_t *)buf);
       buf++;
