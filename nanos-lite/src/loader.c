9785ce8647ff23965af458096a79973dacc8c8d0
tracer-ics2020 <tracer@njuics.org>
2024-10-31 18:19:56 +0800

    >  run 191220000  Static hostname: DESKTOP-75S465I        Icon name: computer-container          Chassis: container       Machine ID: 1c9f91d336034cdebf733748cf9de30a          Boot ID: 1797dea516544bdc89c158d83ddb4a7c   Virtualization: wsl Operating System: Ubuntu 22.04.5 LTS           Kernel: Linux 5.15.153.1-microsoft-standard-WSL2     Architecture: x86-64  18:19:56 up 4 days,  2:03,  1 user,  load average: 0.02, 0.05, 0.03

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,22 +1,43 @@
 #include <proc.h>
 #include <elf.h>
-
+#include <fs.h>
 #ifdef __LP64__
-# define Elf_Ehdr Elf64_Ehdr
-# define Elf_Phdr Elf64_Phdr
+#define Elf_Ehdr Elf64_Ehdr
+#define Elf_Phdr Elf64_Phdr
 #else
-# define Elf_Ehdr Elf32_Ehdr
-# define Elf_Phdr Elf32_Phdr
+#define Elf_Ehdr Elf32_Ehdr
+#define Elf_Phdr Elf32_Phdr
 #endif
 
-static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
+static uintptr_t loader(PCB *pcb, const char *filename)
+{
   return 0;
+  // const char *file_name = filename;
+  // file_name = "/home/mzh/2020PA/ics2020/nanos-lite/build/ramdisk.img";
+  // if (file_name == NULL)
+  // {
+  //   assert(0);
+  //   return 0;
+  // }
+  // int fd;
+  // fd = fs_open(file_name, "rb");
+  // if (fd == -1)
+  // {
+  //   assert(0);
+  //   return 0;
+  // }
+  // Elf_Ehdr ehdr;
+  // if (fs_read(&ehdr, sizeof(Elf32_Ehdr), 1, fd) <= 0)
+  // {
+  //   assert(0);
+  //   return 0;
+  // }
+  // assert(*(uint32_t *)ehdr.e_ident == 0x7f454c46);
 }
 
-void naive_uload(PCB *pcb, const char *filename) {
+void naive_uload(PCB *pcb, const char *filename)
+{
   uintptr_t entry = loader(pcb, filename);
   Log("Jump to entry = %p", entry);
-  ((void(*)())entry) ();
+  ((void (*)())entry)();
 }
-
