babe376d906d8c5e7bc7313761327bbad928ccdb
tracer-ics2020 <tracer@njuics.org>
2024-10-31 20:45:27 +0800

    >  run 191220000  Static hostname: DESKTOP-75S465I        Icon name: computer-container          Chassis: container       Machine ID: 1c9f91d336034cdebf733748cf9de30a          Boot ID: 1797dea516544bdc89c158d83ddb4a7c   Virtualization: wsl Operating System: Ubuntu 22.04.5 LTS           Kernel: Linux 5.15.153.1-microsoft-standard-WSL2     Architecture: x86-64  20:45:27 up 4 days,  4:21,  1 user,  load average: 0.04, 0.03, 0.06

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,11 +1,12 @@
 #include <common.h>
-
+void do_syscall(Context *c);
 static Context *do_event(Event e, Context *c)
 {
   switch (e.event)
   {
   case EVENT_SYSCALL:
     printf("sys get SYSCALL!\n");
+    do_syscall(c);
     break;
   case EVENT_YIELD:
     printf("sys get YEILD!\n");
