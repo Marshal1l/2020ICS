f1b218e0751e203d9b084a7acf7d5c6346616b6f
tracer-ics2020 <tracer@njuics.org>
2024-10-31 22:01:05 +0800

    >  run 191220000  Static hostname: DESKTOP-75S465I        Icon name: computer-container          Chassis: container       Machine ID: 1c9f91d336034cdebf733748cf9de30a          Boot ID: 1797dea516544bdc89c158d83ddb4a7c   Virtualization: wsl Operating System: Ubuntu 22.04.5 LTS           Kernel: Linux 5.15.153.1-microsoft-standard-WSL2     Architecture: x86-64  22:01:05 up 4 days,  5:33,  1 user,  load average: 0.05, 0.07, 0.05

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -1,9 +1,10 @@
 #include <fs.h>
+#include <unistd.h>
+typedef size_t (*ReadFn)(void *buf, size_t offset, size_t len);
+typedef size_t (*WriteFn)(const void *buf, size_t offset, size_t len);
 
-typedef size_t (*ReadFn) (void *buf, size_t offset, size_t len);
-typedef size_t (*WriteFn) (const void *buf, size_t offset, size_t len);
-
-typedef struct {
+typedef struct
+{
   char *name;
   size_t size;
   size_t disk_offset;
@@ -11,26 +12,58 @@ typedef struct {
   WriteFn write;
 } Finfo;
 
-enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB};
+enum
+{
+  FD_STDIN,
+  FD_STDOUT,
+  FD_STDERR,
+  FD_FB
+};
 
-size_t invalid_read(void *buf, size_t offset, size_t len) {
+size_t invalid_read(void *buf, size_t offset, size_t len)
+{
   panic("should not reach here");
   return 0;
 }
 
-size_t invalid_write(const void *buf, size_t offset, size_t len) {
+size_t invalid_write(const void *buf, size_t offset, size_t len)
+{
   panic("should not reach here");
   return 0;
 }
 
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
-  [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write},
-  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, invalid_write},
-  [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
+    [FD_STDIN] = {"stdin", 0, 0, invalid_read, invalid_write},
+    [FD_STDOUT] = {"stdout", 0, 0, invalid_read, invalid_write},
+    [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
 #include "files.h"
 };
 
-void init_fs() {
+void init_fs()
+{
   // TODO: initialize the size of /dev/fb
+}
+size_t fs_write(int fd, const void *buf, size_t len)
+{
+  size_t size = 0;
+  if (fd == 1)
+  {
+    for (int i = 0; i < len && *(const uint8_t *)buf; i++)
+    {
+      putch(*(const uint8_t *)buf);
+      buf++;
+      size++;
+    }
+  }
+  if (fd == 2)
+  {
+    for (int i = 0; i < len && *(const uint8_t *)buf; i++)
+    {
+      putch(*(const uint8_t *)buf);
+      buf++;
+      size++;
+    }
+  }
+  return size;
 }
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,5 +1,6 @@
 #include <common.h>
 #include "syscall.h"
+size_t fs_write(int fd, const void *buf, size_t len);
 void do_syscall(Context *c)
 {
   uintptr_t a[4];
@@ -7,9 +8,12 @@ void do_syscall(Context *c)
   a[1] = c->GPR2;
   a[2] = c->GPR3;
   a[3] = c->GPR4;
-
   switch (a[0])
   {
+  case SYS_write:
+    printf("SYS_WRITE\n");
+    fs_write(a[1], (const void *)a[2], a[3]);
+    break;
   case SYS_yield:
     printf("SYS_YIELD\n");
     yield();
