6965353ee281d5e712f83a128ee1b84ec01d8062
tracer-ics2020 <tracer@njuics.org>
2024-10-31 13:07:27 +0800

    >  compile 191220000  Static hostname: DESKTOP-75S465I        Icon name: computer-container          Chassis: container       Machine ID: 1c9f91d336034cdebf733748cf9de30a          Boot ID: 1797dea516544bdc89c158d83ddb4a7c   Virtualization: wsl Operating System: Ubuntu 22.04.5 LTS           Kernel: Linux 5.15.153.1-microsoft-standard-WSL2     Architecture: x86-64  13:07:27 up 3 days, 21:02,  1 user,  load average: 0.00, 0.02, 0.00

--- nemu/src/isa/x86/exec/system.h
+++ nemu/src/isa/x86/exec/system.h
@@ -7,7 +7,8 @@ void pio_write_l(ioaddr_t, uint32_t);
 void pio_write_w(ioaddr_t, uint32_t);
 void pio_write_b(ioaddr_t, uint32_t);
 
-static inline def_EHelper(lidt) {
+static inline def_EHelper(lidt)
+{
   // TODO();
   vaddr_t addr = *s->isa.mbase;
   cpu.IDTR.limit = vaddr_read(addr, 2);
@@ -15,12 +16,14 @@ static inline def_EHelper(lidt) {
   print_asm_template1(lidt);
 }
 
-static inline def_EHelper(mov_r2cr) {
+static inline def_EHelper(mov_r2cr)
+{
   TODO();
   print_asm("movl %%%s,%%cr%d", reg_name(id_src1->reg, 4), id_dest->reg);
 }
 
-static inline def_EHelper(mov_cr2r) {
+static inline def_EHelper(mov_cr2r)
+{
   TODO();
   print_asm("movl %%cr%d,%%%s", id_src1->reg, reg_name(id_dest->reg, 4));
 
@@ -29,11 +32,13 @@ static inline def_EHelper(mov_cr2r) {
 #endif
 }
 
-static inline def_EHelper(int) {
+static inline def_EHelper(int)
+{
   // TODO();
   rtl_mv(s, t0, ddest);
-  void raise_intr(DecodeExecState *s, uint32_t NO, vaddr_t ret_addr);
+  void raise_intr(DecodeExecState * s, uint32_t NO, vaddr_t ret_addr);
   raise_intr(s, *t0, s->seq_pc);
+  printf("int %s", id_dest->str);
   print_asm("int %s", id_dest->str);
 
 #ifndef __DIFF_REF_NEMU__
@@ -41,7 +46,8 @@ static inline def_EHelper(int) {
 #endif
 }
 
-static inline def_EHelper(iret) {
+static inline def_EHelper(iret)
+{
   // TODO();
   rtl_pop(s, &cpu.pc);
   rtl_pop(s, &cpu.SEGMENT_REG.CS);
@@ -54,26 +60,44 @@ static inline def_EHelper(iret) {
 #endif
 }
 
-static inline def_EHelper(in) {
+static inline def_EHelper(in)
+{
   // TODO();
   rtl_li(s, s0, 0);
-  switch (id_src1->width) {
-    case 1: *s0 = pio_read_b(id_src1->val); break;
-    case 2: *s0 = pio_read_w(id_src1->val); break;
-    case 4: *s0 = pio_read_l(id_src1->val); break;
-    default: panic("Invalid width");
+  switch (id_src1->width)
+  {
+  case 1:
+    *s0 = pio_read_b(id_src1->val);
+    break;
+  case 2:
+    *s0 = pio_read_w(id_src1->val);
+    break;
+  case 4:
+    *s0 = pio_read_l(id_src1->val);
+    break;
+  default:
+    panic("Invalid width");
   }
   operand_write(s, id_dest, s0);
   print_asm_template2(in);
 }
 
-static inline def_EHelper(out) {
+static inline def_EHelper(out)
+{
   // TODO();
-  switch (id_src1->width) {
-    case 1: pio_write_b(id_dest->val, id_src1->val); break;
-    case 2: pio_write_w(id_dest->val, id_src1->val); break;
-    case 4: pio_write_l(id_dest->val, *id_src1->preg); break;
-    default: panic("Invalid width");
+  switch (id_src1->width)
+  {
+  case 1:
+    pio_write_b(id_dest->val, id_src1->val);
+    break;
+  case 2:
+    pio_write_w(id_dest->val, id_src1->val);
+    break;
+  case 4:
+    pio_write_l(id_dest->val, *id_src1->preg);
+    break;
+  default:
+    panic("Invalid width");
   }
   print_asm_template2(out);
 }
