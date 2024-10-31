9f540446a78e1389baa5091d849c10f3a87fb2cb
tracer-ics2020 <tracer@njuics.org>
2024-10-31 22:07:33 +0800

    >  run 191220000  Static hostname: DESKTOP-75S465I        Icon name: computer-container          Chassis: container       Machine ID: 1c9f91d336034cdebf733748cf9de30a          Boot ID: 1797dea516544bdc89c158d83ddb4a7c   Virtualization: wsl Operating System: Ubuntu 22.04.5 LTS           Kernel: Linux 5.15.153.1-microsoft-standard-WSL2     Architecture: x86-64  22:07:33 up 4 days,  5:39,  1 user,  load average: 0.07, 0.11, 0.08

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -12,7 +12,8 @@ void do_syscall(Context *c)
   {
   case SYS_write:
     printf("SYS_WRITE\n");
-    fs_write(a[1], (const void *)a[2], a[3]);
+    printf("fd=%d,len=%d\n", a[1], a[3]);
+    c->GPRx = fs_write(a[1], (const void *)a[2], a[3]);
     break;
   case SYS_yield:
     printf("SYS_YIELD\n");
