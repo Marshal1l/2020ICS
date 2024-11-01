5a7e4c820794155037a04b3f5d66088379937850
tracer-ics2020 <tracer@njuics.org>
2024-11-01 20:56:19 +0800

    >  run 191220000  Static hostname: LAPTOP-LGCRKVO2        Icon name: computer-container          Chassis: container       Machine ID: 86bc4e5bf6ef47fbaa22955b7991ccaa          Boot ID: 28b02e51176d46f99c3454dc4cfa03ac   Virtualization: wsl Operating System: Ubuntu 22.04.3 LTS           Kernel: Linux 5.15.153.1-microsoft-standard-WSL2     Architecture: x86-64  20:56:19 up  3:21,  1 user,  load average: 0.16, 0.05, 0.04

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -33,7 +33,7 @@ static uintptr_t loader(PCB *pcb, const char *filename)
     fs_read(file_id, &phdr, sizeof(Elf_Phdr));
     if (phdr.p_type == PT_LOAD)
     {
-      printf("read PT_LOAD!\n");
+      printf("read PT_LOAD!\n", phdr.p_type);
       fs_lseek(file_id, phdr.p_offset, SEEK_SET);
       fs_read(file_id, (void *)phdr.p_vaddr, phdr.p_filesz);
       memset((void *)(phdr.p_vaddr + phdr.p_filesz), 0, (phdr.p_memsz - phdr.p_filesz));
