a25b7faad24e75cebe4bb1e4b991b44ed2e2d2ee
tracer-ics2020 <tracer@njuics.org>
2024-11-01 20:44:33 +0800

    >  run 191220000  Static hostname: LAPTOP-LGCRKVO2        Icon name: computer-container          Chassis: container       Machine ID: 86bc4e5bf6ef47fbaa22955b7991ccaa          Boot ID: 28b02e51176d46f99c3454dc4cfa03ac   Virtualization: wsl Operating System: Ubuntu 22.04.3 LTS           Kernel: Linux 5.15.153.1-microsoft-standard-WSL2     Architecture: x86-64  20:44:33 up  3:09,  1 user,  load average: 0.17, 0.13, 0.08

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -41,6 +41,7 @@ static uintptr_t loader(PCB *pcb, const char *filename)
 
 void naive_uload(PCB *pcb, const char *filename)
 {
+  printf("load file-- %s\n", filename);
   uintptr_t entry = loader(pcb, filename);
   Log("Jump to entry = %p", entry);
   ((void (*)())entry)();
