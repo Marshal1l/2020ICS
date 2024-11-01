a37027fb628f06eb8ec0a604057f8470452edd54
tracer-ics2020 <tracer@njuics.org>
2024-11-01 21:32:29 +0800

    >  run 191220000  Static hostname: LAPTOP-LGCRKVO2        Icon name: computer-container          Chassis: container       Machine ID: 86bc4e5bf6ef47fbaa22955b7991ccaa          Boot ID: 28b02e51176d46f99c3454dc4cfa03ac   Virtualization: wsl Operating System: Ubuntu 22.04.3 LTS           Kernel: Linux 5.15.153.1-microsoft-standard-WSL2     Architecture: x86-64  21:32:29 up  3:57,  1 user,  load average: 0.16, 0.15, 0.10

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -21,6 +21,9 @@ void do_syscall(Context *c)
   case SYS_open:
     c->GPRx = fs_open((const char *)a[1], a[2], a[3]);
     break;
+  case SYS_close:
+    c->GPRx = fs_close(a[1]);
+    break;
   case SYS_read:
     c->GPRx = fs_read(a[1], (void *)a[2], a[3]);
     break;
--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -61,7 +61,7 @@ void _exit(int status)
 
 int _open(const char *path, int flags, mode_t mode)
 {
-  return _syscall_(SYS_write, (intptr_t)path, flags, (intptr_t)mode);
+  return _syscall_(SYS_open, (intptr_t)path, flags, (intptr_t)mode);
 }
 
 int _write(int fd, void *buf, size_t count)
@@ -88,8 +88,7 @@ int _read(int fd, void *buf, size_t count)
 
 int _close(int fd)
 {
-  _exit(SYS_close);
-  return 0;
+  return _syscall_(SYS_close, fd, 0, 0);
 }
 
 off_t _lseek(int fd, off_t offset, int whence)
