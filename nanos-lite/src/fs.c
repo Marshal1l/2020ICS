0eb4b9fd0670e7adfefc06c3f0e45a9c4618a4d5
tracer-ics2020 <tracer@njuics.org>
2024-11-01 20:33:00 +0800

    >  run 191220000  Static hostname: LAPTOP-LGCRKVO2        Icon name: computer-container          Chassis: container       Machine ID: 86bc4e5bf6ef47fbaa22955b7991ccaa          Boot ID: 28b02e51176d46f99c3454dc4cfa03ac   Virtualization: wsl Operating System: Ubuntu 22.04.3 LTS           Kernel: Linux 5.15.153.1-microsoft-standard-WSL2     Architecture: x86-64  20:33:00 up  2:58,  1 user,  load average: 0.09, 0.09, 0.02

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -1,5 +1,7 @@
 #include <fs.h>
 #include <unistd.h>
+extern size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
 typedef size_t (*ReadFn)(void *buf, size_t offset, size_t len);
 typedef size_t (*WriteFn)(const void *buf, size_t offset, size_t len);
 
@@ -8,6 +10,7 @@ typedef struct
   char *name;
   size_t size;
   size_t disk_offset;
+  size_t open_offset;
   ReadFn read;
   WriteFn write;
 } Finfo;
@@ -34,21 +37,23 @@ size_t invalid_write(const void *buf, size_t offset, size_t len)
 
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
-    [FD_STDIN] = {"stdin", 0, 0, invalid_read, invalid_write},
-    [FD_STDOUT] = {"stdout", 0, 0, invalid_read, invalid_write},
-    [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
+    [FD_STDIN] = {"stdin", 0, 0, 0, invalid_read, invalid_write},
+    [FD_STDOUT] = {"stdout", 0, 0, 0, invalid_read, invalid_write},
+    [FD_STDERR] = {"stderr", 0, 0, 0, invalid_read, invalid_write},
 #include "files.h"
 };
-
+int fs_close(int fd) { return 0; }
 void init_fs()
 {
   // TODO: initialize the size of /dev/fb
 }
 size_t fs_write(int fd, const void *buf, size_t len)
 {
+  if (len == 0)
+    return 0;
   size_t size = 0;
   const void *tmpbuf = buf;
-  if (fd == 1)
+  if (fd == 1 || fd == 2)
   {
     for (int i = 0; i < len && *(const uint8_t *)tmpbuf != '\0'; i++)
     {
@@ -56,6 +61,66 @@ size_t fs_write(int fd, const void *buf, size_t len)
       tmpbuf++;
       size++;
     }
+    return size;
   }
+  Finfo des_file = file_table[fd];
+  size_t write_pos = des_file.disk_offset + des_file.open_offset;
+  if (write_pos + len > des_file.disk_offset + des_file.size)
+  {
+    panic("write out of the file!\n");
+    return -1;
+  }
+  if ((size = ramdisk_write(tmpbuf, write_pos, len)) < 0)
+  {
+    panic("write file error!\n");
+    return -1;
+  }
+  des_file.open_offset += size;
   return size;
+}
+size_t fs_read(int fd, void *buf, size_t len)
+{
+  if (len == 0)
+    return 0;
+  Finfo des_file = file_table[fd];
+  size_t size = 0;
+  size_t read_pos = des_file.disk_offset + des_file.open_offset;
+  if (read_pos + len > des_file.disk_offset + des_file.size)
+  {
+    panic("read out of the file!\n");
+    return -1;
+  }
+  if ((size = ramdisk_read(buf, read_pos, len)) < 0)
+  {
+    panic("read file error!\n");
+    return -1;
+  }
+  des_file.open_offset += size;
+  return size;
+}
+size_t fs_lseek(int fd, size_t offset, int whence)
+{
+  Finfo des_file = file_table[fd];
+  if (whence == SEEK_SET)
+  {
+    des_file.open_offset = offset;
+    return des_file.open_offset;
+  }
+  if (whence == SEEK_CUR)
+  {
+    if ((des_file.open_offset = des_file.open_offset + offset) > des_file.size)
+    {
+      return -1;
+    }
+    return des_file.open_offset;
+  }
+  if (whence == SEEK_END)
+  {
+    if ((des_file.open_offset = des_file.size + offset) > des_file.size)
+    {
+      return -1;
+    }
+    return des_file.open_offset;
+  }
+  return -1;
 }
