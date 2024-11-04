fc096da4df5591806ff74d4af338e5792700a512
tracer-ics2020 <tracer@njuics.org>
2024-11-04 21:36:46 +0800

    >  run 191220000  Static hostname: LAPTOP-LGCRKVO2        Icon name: computer-container          Chassis: container       Machine ID: 86bc4e5bf6ef47fbaa22955b7991ccaa          Boot ID: 40f5e2ab36c3444f8a161e6b8a1bcd60   Virtualization: wsl Operating System: Ubuntu 22.04.3 LTS           Kernel: Linux 5.15.153.1-microsoft-standard-WSL2     Architecture: x86-64  21:36:46 up  2:22,  1 user,  load average: 0.31, 0.28, 0.13

--- navy-apps/tests/time-test/time.c
+++ navy-apps/tests/time-test/time.c
@@ -6,16 +6,16 @@ int main()
 {
   struct timeval num;
   num.tv_sec = 0;
-  printf("time-test starts begin = %d\n", num.tv_sec);
+  printf("time-test starts begin = %ld\n", num.tv_sec);
   int sec = 1;
   while (1)
   {
     while (num.tv_sec <= sec)
     {
-      int t = _gettimeofday(&num, NULL);
+      int t = gettimeofday(&num, NULL);
       assert(t == 0);
     }
-    printf("time: seconds = %d   sec=%d usec%d\n", sec, num.tv_sec, num.tv_usec);
+    printf("time: seconds = %d   sec=%ld usec%ld\n", sec, num.tv_sec, num.tv_usec);
     sec++;
   }
 }
