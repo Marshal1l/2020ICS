54c5c664896ba9cf78b8c2688ab9884106e41705
tracer-ics2020 <tracer@njuics.org>
2024-11-05 21:16:45 +0800

    >  run 191220000  Static hostname: LAPTOP-LGCRKVO2        Icon name: computer-container          Chassis: container       Machine ID: 86bc4e5bf6ef47fbaa22955b7991ccaa          Boot ID: cdd5ca0bb1134ba997023872354547a5   Virtualization: wsl Operating System: Ubuntu 22.04.3 LTS           Kernel: Linux 5.15.153.1-microsoft-standard-WSL2     Architecture: x86-64  21:16:45 up 17 min,  1 user,  load average: 0.21, 0.26, 0.15

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -97,10 +97,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len)
 {
   int w = io_read(AM_GPU_CONFIG).width;
   int h = io_read(AM_GPU_CONFIG).height;
-  int res = sprintf(buf, "%d%d", w, h);
-  printf("disp  buf %s\n", buf);
-  printf("disp read W:%d H:%d\n", w, h);
-
+  int res = sprintf(buf, "%d\n%d", w, h);
   return res;
 }
 
