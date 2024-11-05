300387a112976ef38de1347f19c1592b8e08fb4d
tracer-ics2020 <tracer@njuics.org>
2024-11-05 21:53:03 +0800

    >  run 191220000  Static hostname: LAPTOP-LGCRKVO2        Icon name: computer-container          Chassis: container       Machine ID: 86bc4e5bf6ef47fbaa22955b7991ccaa          Boot ID: cdd5ca0bb1134ba997023872354547a5   Virtualization: wsl Operating System: Ubuntu 22.04.3 LTS           Kernel: Linux 5.15.153.1-microsoft-standard-WSL2     Architecture: x86-64  21:53:03 up 54 min,  1 user,  load average: 0.28, 0.24, 0.16

--- navy-apps/tests/bmp-test/main.c
+++ navy-apps/tests/bmp-test/main.c
@@ -10,8 +10,9 @@ int main()
   int w, h;
   void *bmp = BMP_Load("/share/pictures/projectn.bmp", &w, &h);
   assert(bmp);
-  NDL_OpenCanvas(&w, &h);
   printf("wwwwwwwwwwwhhhhhhhhhhhhhhhhh:=%d\t%d\n", w, h);
+  NDL_OpenCanvas(&w, &h);
+
   NDL_DrawRect(bmp, 0, 0, w, h);
   free(bmp);
   NDL_Quit();
