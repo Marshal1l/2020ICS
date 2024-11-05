04f03b8747eafa5876e32d5dc1e31cc61257aa67
tracer-ics2020 <tracer@njuics.org>
2024-11-05 15:48:58 +0800

    >  run 191220000  Static hostname: LAPTOP-LGCRKVO2        Icon name: computer-container          Chassis: container       Machine ID: 86bc4e5bf6ef47fbaa22955b7991ccaa          Boot ID: 67cfbb629aff4b6baf082b604f44b614   Virtualization: wsl Operating System: Ubuntu 22.04.3 LTS           Kernel: Linux 5.15.153.1-microsoft-standard-WSL2     Architecture: x86-64  15:48:58 up 37 min,  1 user,  load average: 0.12, 0.07, 0.04

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -1,10 +1,14 @@
 #include <fs.h>
 #include <unistd.h>
+
+typedef size_t (*ReadFn)(void *buf, size_t offset, size_t len);
+extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
+extern size_t events_read(void *buf, size_t offset, size_t len);
+extern size_t dispinfo_read(void *buf, size_t offset, size_t len);
+
+typedef size_t (*WriteFn)(const void *buf, size_t offset, size_t len);
 extern size_t serial_write(const void *buf, size_t offset, size_t len);
 extern size_t ramdisk_write(const void *buf, size_t offset, size_t len);
-extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
-typedef size_t (*ReadFn)(void *buf, size_t offset, size_t len);
-typedef size_t (*WriteFn)(const void *buf, size_t offset, size_t len);
 
 typedef struct
 {
@@ -38,10 +42,14 @@ size_t invalid_write(const void *buf, size_t offset, size_t len)
 
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
+
     [FD_STDIN] = {"stdin", 0, 0, 0, invalid_read, invalid_write},
     [FD_STDOUT] = {"stdout", 0, 0, 0, invalid_read, serial_write},
     [FD_STDERR] = {"stderr", 0, 0, 0, invalid_read, serial_write},
+    {"/dev/events", 0, 0, 0, events_read, invalid_write},
+    {"/proc/dispinfo", 0, 0, 0, dispinfo_read, invalid_write},
 #include "files.h"
+
 };
 int fs_open(const char *pathname, int flags, int mode)
 {
@@ -56,7 +64,10 @@ int fs_open(const char *pathname, int flags, int mode)
   panic("open file error! filename=:%s\n", pathname);
   return -1;
 }
-int fs_close(int fd) { return 0; }
+int fs_close(int fd)
+{
+  return 0;
+}
 void init_fs()
 {
   // TODO: initialize the size of /dev/fb
