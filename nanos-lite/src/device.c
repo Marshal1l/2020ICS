29794ba1f03fd9a1973803b8488da6d1e810d4e1
tracer-ics2020 <tracer@njuics.org>
2024-11-05 10:56:10 +0800

    >  run 191220000  Static hostname: LAPTOP-LGCRKVO2        Icon name: computer-container          Chassis: container       Machine ID: 86bc4e5bf6ef47fbaa22955b7991ccaa          Boot ID: 31d3570734b9400d860d5afa85bc2ea8   Virtualization: wsl Operating System: Ubuntu 22.04.3 LTS           Kernel: Linux 5.15.153.1-microsoft-standard-WSL2     Architecture: x86-64  10:56:10 up 36 min,  1 user,  load average: 0.33, 0.26, 0.16

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -17,8 +17,8 @@ int sys_gettimeofday(struct timeval *tv, struct timezone *tz)
 {
   AM_TIMER_UPTIME_T uptime;
   uptime = io_read(AM_TIMER_UPTIME);
-  int sec = tv->tv_sec = uptime.us >> 32;
-  tv->tv_usec = (uint64_t)uptime.us + ((uint64_t)sec << 32);
+  tv->tv_sec = uptime.us >> 32;
+  tv->tv_usec = (uint64_t)uptime.us;
   tz = NULL;
   return 0;
 }
