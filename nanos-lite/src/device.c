795c2af350c77ac42499fd2aa19a864a2a51a080
tracer-ics2020 <tracer@njuics.org>
2024-11-05 17:11:53 +0800

    >  run 191220000  Static hostname: LAPTOP-LGCRKVO2        Icon name: computer-container          Chassis: container       Machine ID: 86bc4e5bf6ef47fbaa22955b7991ccaa          Boot ID: 67cfbb629aff4b6baf082b604f44b614   Virtualization: wsl Operating System: Ubuntu 22.04.3 LTS           Kernel: Linux 5.15.153.1-microsoft-standard-WSL2     Architecture: x86-64  17:11:53 up  2:00,  1 user,  load average: 0.12, 0.13, 0.06

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -1,6 +1,9 @@
 #include <common.h>
 #include <sys/time.h>
 
+#include <common.h>
+#include <fs.h>
+
 #if defined(MULTIPROGRAM) && !defined(TIME_SHARING)
 #define MULTIPROGRAM_YIELD() yield()
 #else
@@ -13,6 +16,66 @@
 static const char *keyname[256] __attribute__((used)) = {
     [AM_KEY_NONE] = "NONE",
     AM_KEYS(NAME)};
+
+size_t serial_write(const void *buf, size_t offset, size_t len)
+{
+#ifndef HAS_TIMER_IRQ
+  yield();
+#endif
+
+  for (int i = 0; i < len; i++)
+  {
+    putch((((char *)(buf))[i]));
+  }
+  return len;
+}
+#define KEYDOWN_MASK 0x8000
+#define KBD_ADDR 0xa1000060
+static inline uint32_t inl(int port)
+{
+  uint32_t data;
+  asm volatile("inl %1, %0" : "=a"(data) : "d"((uint16_t)port));
+  return data;
+}
+
+// void change_fg(int i);
+
+size_t events_read(void *buf, size_t offset, size_t len)
+{
+
+#ifndef HAS_TIMER_IRQ
+  yield();
+#endif
+
+  AM_INPUT_KEYBRD_T kbd;
+  int k = inl(KBD_ADDR);
+  kbd.keydown = (k & KEYDOWN_MASK ? true : false);
+  kbd.keycode = k & ~KEYDOWN_MASK;
+  if (kbd.keycode == AM_KEY_NONE)
+    return 0;
+  else
+  {
+    char ku[] = "ku";
+    char kd[] = "kd";
+    char *kstate = (char *)((kbd.keydown) ? &kd : &ku);
+    int res = sprintf(buf, "%s %s\n", kstate, keyname[kbd.keycode]);
+
+    // if (kbd.keycode == AM_KEY_F1)
+    // {
+    //   change_fg(1);
+    // }
+    // if (kbd.keycode == AM_KEY_F2)
+    // {
+    //   change_fg(2);
+    // }
+    // if (kbd.keycode == AM_KEY_F3)
+    // {
+    //   change_fg(3);
+    // }
+    // printf("event : %s\n",buf);
+    return res;
+  }
+}
 int sys_gettimeofday(struct timeval *tv, struct timezone *tz)
 {
   AM_TIMER_UPTIME_T uptime;
@@ -21,28 +84,6 @@ int sys_gettimeofday(struct timeval *tv, struct timezone *tz)
   tv->tv_usec = (uint64_t)uptime.us - ((uint64_t)sec << 32);
   tz = NULL;
   return 0;
-}
-size_t serial_write(const void *buf, size_t offset, size_t len)
-{
-  for (int i = 0; i < len; i++)
-  {
-    putch(*(const uint8_t *)buf);
-    buf++;
-  }
-  return len;
-}
-
-size_t events_read(void *buf, size_t offset, size_t len)
-{
-  AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
-  if (ev.keycode == AM_KEY_NONE)
-  {
-    *(char *)buf = '\0';
-    return 0;
-  }
-  int ret = snprintf(buf, len, "%s %s\n", ev.keydown ? "kd" : "ku", keyname[ev.keycode]);
-  printf("%s\n", buf);
-  return ret;
 }
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len)
