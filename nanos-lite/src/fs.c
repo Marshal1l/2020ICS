8c1e10e84d60eb4c6e1fc4fdaef3a37abebcf146
tracer-ics2020 <tracer@njuics.org>
2024-11-05 16:19:32 +0800

    >  run 191220000  Static hostname: LAPTOP-LGCRKVO2        Icon name: computer-container          Chassis: container       Machine ID: 86bc4e5bf6ef47fbaa22955b7991ccaa          Boot ID: 67cfbb629aff4b6baf082b604f44b614   Virtualization: wsl Operating System: Ubuntu 22.04.3 LTS           Kernel: Linux 5.15.153.1-microsoft-standard-WSL2     Architecture: x86-64  16:19:32 up  1:08,  1 user,  load average: 0.03, 0.08, 0.08

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -135,8 +135,7 @@ size_t fs_read(int fd, void *buf, size_t len)
   else
   {
     printf("read from key\n");
-    int ret = file_table[fd].read(buf, file_table[fd].open_offset, len);
-    file_table[fd].open_offset += len;
+    int ret = file_table[fd].read(buf, 0, len);
     return ret;
   }
 }
