987e6a0a3a8263ab1bb0bea6b1c6647492b344fb
tracer-ics2020 <tracer@njuics.org>
2024-11-05 16:06:11 +0800

    >  run 191220000  Static hostname: LAPTOP-LGCRKVO2        Icon name: computer-container          Chassis: container       Machine ID: 86bc4e5bf6ef47fbaa22955b7991ccaa          Boot ID: 67cfbb629aff4b6baf082b604f44b614   Virtualization: wsl Operating System: Ubuntu 22.04.3 LTS           Kernel: Linux 5.15.153.1-microsoft-standard-WSL2     Architecture: x86-64  16:06:11 up 55 min,  1 user,  load average: 0.18, 0.17, 0.10

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -134,11 +134,9 @@ size_t fs_read(int fd, void *buf, size_t len)
   }
   else
   {
-    size_t l = len <= file_table[fd].size - file_table[fd].open_offset ? len : file_table[fd].size - file_table[fd].open_offset;
-    ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, l);
-    file_table[fd].open_offset = file_table[fd].open_offset + l;
-    assert(file_table[fd].open_offset <= file_table[fd].size);
-    return l;
+    int ret = file_table[fd].read(buf, file_table[fd].open_offset, len);
+    file_table[fd].open_offset += len;
+    return ret;
   }
 }
 size_t fs_lseek(int fd, size_t offset, int whence)
