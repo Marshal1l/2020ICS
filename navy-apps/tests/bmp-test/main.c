9b58e3ba28d470f0956bc7612039e5ac5d08dbe7
tracer-ics2020 <tracer@njuics.org>
2024-11-05 21:52:43 +0800

    >  run 191220000  Static hostname: LAPTOP-LGCRKVO2        Icon name: computer-container          Chassis: container       Machine ID: 86bc4e5bf6ef47fbaa22955b7991ccaa          Boot ID: cdd5ca0bb1134ba997023872354547a5   Virtualization: wsl Operating System: Ubuntu 22.04.3 LTS           Kernel: Linux 5.15.153.1-microsoft-standard-WSL2     Architecture: x86-64  21:52:43 up 53 min,  1 user,  load average: 0.20, 0.22, 0.15

--- navy-apps/tests/bmp-test/main.c
+++ navy-apps/tests/bmp-test/main.c
@@ -4,16 +4,19 @@
 #include <NDL.h>
 #include <BMP.h>
 
-int main() {
+int main()
+{
   NDL_Init(0);
   int w, h;
   void *bmp = BMP_Load("/share/pictures/projectn.bmp", &w, &h);
   assert(bmp);
   NDL_OpenCanvas(&w, &h);
+  printf("wwwwwwwwwwwhhhhhhhhhhhhhhhhh:=%d\t%d\n", w, h);
   NDL_DrawRect(bmp, 0, 0, w, h);
   free(bmp);
   NDL_Quit();
   printf("Test ends! Spinning...\n");
-  while (1);
+  while (1)
+    ;
   return 0;
 }
