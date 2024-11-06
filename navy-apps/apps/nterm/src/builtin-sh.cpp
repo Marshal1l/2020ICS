794debe54fcc4c51dbbaef89ad924b984ef60eb7
tracer-ics2020 <tracer@njuics.org>
2024-11-06 16:34:26 +0800

    >  run 191220000  Static hostname: LAPTOP-LGCRKVO2        Icon name: computer-container          Chassis: container       Machine ID: 86bc4e5bf6ef47fbaa22955b7991ccaa          Boot ID: 1423602b81034814a08a54b7d1c60ea2   Virtualization: wsl Operating System: Ubuntu 22.04.3 LTS           Kernel: Linux 5.15.153.1-microsoft-standard-WSL2     Architecture: x86-64  16:34:26 up  2:18,  1 user,  load average: 0.98, 0.73, 0.45

--- navy-apps/apps/nterm/src/builtin-sh.cpp
+++ navy-apps/apps/nterm/src/builtin-sh.cpp
@@ -5,7 +5,8 @@
 
 char handle_key(SDL_Event *ev);
 
-static void sh_printf(const char *format, ...) {
+static void sh_printf(const char *format, ...)
+{
   static char buf[256] = {};
   va_list ap;
   va_start(ap, format);
@@ -14,27 +15,36 @@ static void sh_printf(const char *format, ...) {
   term->write(buf, len);
 }
 
-static void sh_banner() {
+static void sh_banner()
+{
   sh_printf("Built-in Shell in NTerm (NJU Terminal)\n\n");
 }
 
-static void sh_prompt() {
+static void sh_prompt()
+{
   sh_printf("sh> ");
 }
 
-static void sh_handle_cmd(const char *cmd) {
+static void sh_handle_cmd(const char *cmd)
+{
 }
 
-void builtin_sh_run() {
+void builtin_sh_run()
+{
   sh_banner();
   sh_prompt();
 
-  while (1) {
+  while (1)
+  {
     SDL_Event ev;
-    if (SDL_PollEvent(&ev)) {
-      if (ev.type == SDL_KEYUP || ev.type == SDL_KEYDOWN) {
+    if (SDL_PollEvent(&ev))
+    {
+      if (ev.type == SDL_KEYUP || ev.type == SDL_KEYDOWN)
+      {
         const char *res = term->keypress(handle_key(&ev));
-        if (res) {
+        if (res)
+        {
+          printf("res:=%s\n", res);
           sh_handle_cmd(res);
           sh_prompt();
         }
