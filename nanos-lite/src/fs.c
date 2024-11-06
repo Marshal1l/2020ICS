1a9219e31954919f1df379644c7da188725e0243
tracer-ics2020 <tracer@njuics.org>
2024-11-06 17:02:26 +0800

    >  run 191220000  Static hostname: LAPTOP-LGCRKVO2        Icon name: computer-container          Chassis: container       Machine ID: 86bc4e5bf6ef47fbaa22955b7991ccaa          Boot ID: 1423602b81034814a08a54b7d1c60ea2   Virtualization: wsl Operating System: Ubuntu 22.04.3 LTS           Kernel: Linux 5.15.153.1-microsoft-standard-WSL2     Architecture: x86-64  17:02:26 up  2:46,  1 user,  load average: 0.02, 0.17, 0.28

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -71,7 +71,7 @@ size_t fs_open(const char *pathname, int flags, int mode)
 {
   for (int i = 0; i < FILENUM; i++)
   {
-    printf("path:=%s,this:=%s\n", pathname, file_table[i].name);
+    // printf("path:=%s,this:=%s\n", pathname, file_table[i].name);
     if (strcmp(pathname, file_table[i].name) == 0)
     {
       // printf("find the file%s\n", pathname);
