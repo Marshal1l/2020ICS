7a92b530139c5e38ed7dcb98ff6adede1bcaa698
tracer-ics2020 <tracer@njuics.org>
2024-11-06 15:43:10 +0800

    >  run 191220000  Static hostname: LAPTOP-LGCRKVO2        Icon name: computer-container          Chassis: container       Machine ID: 86bc4e5bf6ef47fbaa22955b7991ccaa          Boot ID: 1423602b81034814a08a54b7d1c60ea2   Virtualization: wsl Operating System: Ubuntu 22.04.3 LTS           Kernel: Linux 5.15.153.1-microsoft-standard-WSL2     Architecture: x86-64  15:43:10 up  1:27,  1 user,  load average: 0.32, 0.16, 0.11

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -1,7 +1,7 @@
 #include <NDL.h>
 #include <SDL.h>
-#include <assert.h>
 #include <string.h>
+#include <assert.h>
 
 #define keyname(k) #k,
 
@@ -13,110 +13,70 @@ static uint8_t keystate[83] = {0};
 
 int SDL_PushEvent(SDL_Event *ev)
 {
-  printf("please implement me\n");
-  assert(0);
   return 0;
 }
 
 int SDL_PollEvent(SDL_Event *ev)
 {
-  // printf("in\n");
-  char key[64] = {0};
-  key[0] = '\0';
-  int keycode = 0;
-  int ret = NDL_PollEvent(key, sizeof(key));
-  // printf("SDL %s\n",key);
-  if (key[0] == '\0')
+  int len = 0;
+  char buf[64];
+  len = NDL_PollEvent(buf, 64);
+
+  if (len == 0)
   {
-    ev->key.keysym.sym = SDLK_NONE;
-    ev->type = SDL_USEREVENT;
     return 0;
   }
   else
   {
-    char *str = key + 3;
-    // printf("%s %d\n",key,strlen(key));
-    for (int i = 0, cnt = 0; i < sizeof(keyname); i += strlen(keyname[cnt]), cnt++)
+    // printf("%s\n",buf);
+    if (buf[0] == 'k' && buf[1] == 'u')
     {
-      // printf("start %d %s %s %d %d %d end\n",cnt,keyname[cnt],str,strlen(str),!strncmp(keyname[cnt],str,strlen(str)),i);
-      if (!strncmp(keyname[cnt], str, strlen(str)) && !strncmp(keyname[cnt], str, strlen(keyname[cnt])))
-      {
-        keycode = cnt;
-        break;
-      }
-    }
-    // printf("in waitevent %d\n",keycode);
-    if (key[1] == 'u')
-    {
-      ev->key.keysym.sym = keycode;
       ev->type = SDL_KEYUP;
-      keystate[keycode] = 0;
     }
-    else if (key[1] == 'd')
+    else if (buf[0] == 'k' && buf[1] == 'd')
     {
-      ev->key.keysym.sym = keycode;
       ev->type = SDL_KEYDOWN;
-      keystate[keycode] = 1;
     }
     else
+    {
       assert(0);
-    // printf("please implement me\n");
+    }
+    char buf2[16];
+    strcpy(buf2, buf + 3);
+    buf2[strlen(buf2) - 1] = '\0';
+
+    for (int i = 0; i < 83; i++)
+    {
+      // printf("%d [%s]||||[%s]||||\n",i,keyname[i],buf2);
+      if (strcmp((const char *)buf2, (const char *)keyname[i]) == 0)
+      {
+        ev->key.keysym.sym = i;
+        // printf("%s",buf2);
+
+        keystate[i] = (ev->type == SDL_KEYDOWN);
+        return 1;
+      }
+    }
+    // return 1;
     // assert(0);
-    return 1;
   }
 }
 
 int SDL_WaitEvent(SDL_Event *event)
 {
-  char key[64] = {0};
-  memset(key, 0, sizeof(key));
-  key[0] = '0';
-  int keycode = 0;
-  while (!NDL_PollEvent(key, sizeof(key)))
+
+  while (!SDL_PollEvent(event))
     ;
-  // printf("%s\n",key);
-  char *str = key + 3;
-  for (int i = 0, cnt = 0; i < sizeof(keyname); i += sizeof(keyname[cnt]), cnt += 1)
-  {
-    if (!strncmp(keyname[cnt], str, strlen(str)) && !strncmp(keyname[cnt], str, strlen(keyname[cnt])))
-    {
-      keycode = cnt;
-      break;
-    }
-    assert(i <= sizeof(keyname));
-  }
-  // printf("in waitevent\n");
-  if (key[1] == 'u')
-  {
-    event->key.keysym.sym = keycode;
-    event->type = SDL_KEYUP;
-    keystate[keycode] = 0;
-  }
-  else if (key[1] == 'd')
-  {
-    event->key.keysym.sym = keycode;
-    event->type = SDL_KEYDOWN;
-    keystate[keycode] = 1;
-  }
-  else
-    assert(0);
-  return 0;
+  return 1;
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask)
 {
-  printf("please implement me\n");
   assert(0);
   return 0;
 }
-
 uint8_t *SDL_GetKeyState(int *numkeys)
 {
-  if (numkeys == NULL)
-    return keystate;
-  else
-    assert(0);
-  // printf("please implement me\n");
-  // assert(0);
-  // else assert(0);
+
+  return keystate;
 }
