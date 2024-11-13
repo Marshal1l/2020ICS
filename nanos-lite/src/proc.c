cd498a5801ba9646d03aff66195cd53a083a24d9
tracer-ics2020 <tracer@njuics.org>
2024-11-13 16:52:18 +0800

    >  run 191220000  Static hostname: DESKTOP-75S465I        Icon name: computer-container          Chassis: container       Machine ID: 1c9f91d336034cdebf733748cf9de30a          Boot ID: 874e68537c6043809d49fa582a23d155   Virtualization: wsl Operating System: Ubuntu 22.04.5 LTS           Kernel: Linux 5.15.153.1-microsoft-standard-WSL2     Architecture: x86-64  16:52:18 up 5 days, 19:14,  1 user,  load average: 0.24, 0.12, 0.08

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -33,7 +33,7 @@ void init_proc()
   // context_kload(&pcb[1], hello_fun, "V");
   char *argvs[] = {"123", "456", NULL};
   char *envs[] = {"evir", "there", NULL};
-  context_uload(&pcb[0], "/bin/bird", argvs, envs);
+  context_uload(&pcb[0], "/bin/exec-test", argvs, envs);
   switch_boot_pcb();
 
   // load program here
