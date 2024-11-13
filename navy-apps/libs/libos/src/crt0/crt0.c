264352d577c9c95dc57acd3a974d4a6c23c4f02d
tracer-ics2020 <tracer@njuics.org>
2024-11-13 15:33:56 +0800

    >  run 191220000  Static hostname: DESKTOP-75S465I        Icon name: computer-container          Chassis: container       Machine ID: 1c9f91d336034cdebf733748cf9de30a          Boot ID: 874e68537c6043809d49fa582a23d155   Virtualization: wsl Operating System: Ubuntu 22.04.5 LTS           Kernel: Linux 5.15.153.1-microsoft-standard-WSL2     Architecture: x86-64  15:33:56 up 5 days, 17:59,  1 user,  load average: 0.09, 0.08, 0.04

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -1,12 +1,15 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <assert.h>
-
+#include <stdio.h>
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
-void call_main(uintptr_t *args) {
-  char *empty[] =  {NULL };
+void call_main(uintptr_t *args)
+{
+  char *empty[] = {NULL};
   environ = empty;
+  int argc = *args;
+  printf("argc:=%d\n", argc);
   exit(main(0, empty, empty));
   assert(0);
 }
