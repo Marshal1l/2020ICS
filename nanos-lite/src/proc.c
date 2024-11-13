429f7cff3719452e978dd46ba648844c49b385a3
tracer-ics2020 <tracer@njuics.org>
2024-11-13 17:21:09 +0800

    >  run 191220000  Static hostname: DESKTOP-75S465I        Icon name: computer-container          Chassis: container       Machine ID: 1c9f91d336034cdebf733748cf9de30a          Boot ID: 874e68537c6043809d49fa582a23d155   Virtualization: wsl Operating System: Ubuntu 22.04.5 LTS           Kernel: Linux 5.15.153.1-microsoft-standard-WSL2     Architecture: x86-64  17:21:09 up 5 days, 19:41,  1 user,  load average: 0.28, 0.24, 0.18

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -9,7 +9,7 @@ PCB *current = NULL;
 void switch_boot_pcb()
 {
   current = &pcb_boot;
-  printf("switch_boot_pcb current %d\n", current);
+  printf("switch_boot_pcb current %x\n", current);
 }
 
 void hello_fun(void *arg)
