99ddeecf46132a68b6df5004f0b39401a98aa9d8
tracer-ics2020 <tracer@njuics.org>
2024-11-15 13:26:04 +0800

    >  compile 191220000  Static hostname: DESKTOP-75S465I        Icon name: computer-container          Chassis: container       Machine ID: 1c9f91d336034cdebf733748cf9de30a          Boot ID: fc70c37418b34f77b77ca89cc0e0f9bc   Virtualization: wsl Operating System: Ubuntu 22.04.5 LTS           Kernel: Linux 5.15.167.4-microsoft-standard-WSL2     Architecture: x86-64  13:26:04 up 19:29,  1 user,  load average: 0.16, 0.12, 0.04

--- nemu/src/isa/x86/local-include/reg.h
+++ nemu/src/isa/x86/local-include/reg.h
@@ -53,8 +53,14 @@ static inline const char *reg_name(int index, int width)
   extern const char *regsl[];
   extern const char *regsw[];
   extern const char *regsb[];
-  assert(index >= 0 && index < 8);
-
+  if (index >= 0 && index < 8)
+  {
+  }
+  else
+  {
+    printf("%x\n", index);
+    assert(index >= 0 && index < 8);
+  }
   switch (width)
   {
   case 4:
