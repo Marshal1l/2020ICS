8f982b635768ae6c42c2e63b14104a7c5f176be8
tracer-ics2020 <tracer@njuics.org>
2024-11-15 16:20:04 +0800

    >  compile 191220000  Static hostname: DESKTOP-75S465I        Icon name: computer-container          Chassis: container       Machine ID: 1c9f91d336034cdebf733748cf9de30a          Boot ID: fc70c37418b34f77b77ca89cc0e0f9bc   Virtualization: wsl Operating System: Ubuntu 22.04.5 LTS           Kernel: Linux 5.15.167.4-microsoft-standard-WSL2     Architecture: x86-64  16:20:04 up 22:17,  1 user,  load average: 0.50, 0.20, 0.08

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -136,7 +136,7 @@ void init_monitor(int argc, char *argv[])
 
   /* Perform ISA dependent initialization. */
   init_isa();
-  printf("cpu.cr3:=%x", cpu.cr3);
+
   /* Load the image to memory. This will overwrite the built-in image. */
   long img_size = load_img();
 
@@ -148,7 +148,7 @@ void init_monitor(int argc, char *argv[])
 
   /* Initialize differential testing. */
   init_difftest(diff_so_file, img_size, difftest_port);
-
+  printf("cpu.cr3:=%x", cpu.cr3);
   /* Display welcome message. */
   welcome();
 }
